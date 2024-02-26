9fa394f6cacbe972a269cbaf0acb65860ec882db
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-02-26 15:21:30 +0100

    feat: complete quad splitting benchmarks

--- honeycomb-utils/benches/squaremap/split.rs
+++ honeycomb-utils/benches/squaremap/split.rs
@@ -5,23 +5,89 @@ use rand::{
     SeedableRng,
 };
 
-use honeycomb_core::TwoMap;
+use honeycomb_core::{DartIdentifier, SewPolicy, TwoMap, UnsewPolicy};
 use honeycomb_utils::generation::square_two_map;
 
+const N_SQUARE: usize = 2_usize.pow(11);
+
 fn split<const N_MARKS: usize>(mut map: TwoMap<N_MARKS>) {
+    (0..N_SQUARE.pow(2)).for_each(|square| {
+        let d1 = (1 + square * 4) as DartIdentifier;
+        let (d2, d3, d4) = (d1 + 1, d1 + 2, d1 + 3);
+        // in a parallel impl, we would create all new darts before-hand
+        let dsplit1 = map.add_free_darts(2);
+        let dsplit2 = dsplit1 + 1;
+        map.two_sew(dsplit1, dsplit2, SewPolicy::StretchLeft);
+        map.one_unsew(d1, UnsewPolicy::DoNothing);
+        map.one_unsew(d3, UnsewPolicy::DoNothing);
+        map.one_sew(d1, dsplit1, SewPolicy::StretchLeft);
+        map.one_sew(d3, dsplit2, SewPolicy::StretchLeft);
+        map.one_sew(dsplit1, d4, SewPolicy::StretchRight);
+        map.one_sew(dsplit2, d2, SewPolicy::StretchRight);
+    });
+
+    // rebuild faces
+    assert_eq!(map.build_all_faces(), N_SQUARE.pow(2) * 2);
+
     black_box(&mut map);
 }
 
 fn split_some<const N_MARKS: usize>(mut map: TwoMap<N_MARKS>, split: &[bool]) {
+    (0..N_SQUARE.pow(2))
+        .filter(|square| split[*square]) // split only if true
+        .for_each(|square| {
+            let d1 = (1 + square * 4) as DartIdentifier;
+            let (d2, d3, d4) = (d1 + 1, d1 + 2, d1 + 3);
+            // in a parallel impl, we would create all new darts before-hand
+            let dsplit1 = map.add_free_darts(2);
+            let dsplit2 = dsplit1 + 1;
+            map.two_sew(dsplit1, dsplit2, SewPolicy::StretchLeft);
+            map.one_unsew(d1, UnsewPolicy::DoNothing);
+            map.one_unsew(d3, UnsewPolicy::DoNothing);
+            map.one_sew(d1, dsplit1, SewPolicy::StretchLeft);
+            map.one_sew(d3, dsplit2, SewPolicy::StretchLeft);
+            map.one_sew(dsplit1, d4, SewPolicy::StretchRight);
+            map.one_sew(dsplit2, d2, SewPolicy::StretchRight);
+        });
+
+    // rebuild faces
+    assert_eq!(map.build_all_faces(), N_SQUARE.pow(2) * 2);
+
     black_box(&mut map);
 }
 
 fn split_diff<const N_MARKS: usize>(mut map: TwoMap<N_MARKS>, split: &[bool]) {
+    (0..N_SQUARE.pow(2)).for_each(|square| {
+        let d1 = (1 + square * 4) as DartIdentifier;
+        let (d2, d3, d4) = (d1 + 1, d1 + 2, d1 + 3);
+        // in a parallel impl, we would create all new darts before-hand
+        let dsplit1 = map.add_free_darts(2);
+        let dsplit2 = dsplit1 + 1;
+        map.two_sew(dsplit1, dsplit2, SewPolicy::StretchLeft);
+        if split[square] {
+            map.one_unsew(d1, UnsewPolicy::DoNothing);
+            map.one_unsew(d3, UnsewPolicy::DoNothing);
+            map.one_sew(d1, dsplit1, SewPolicy::StretchLeft);
+            map.one_sew(d3, dsplit2, SewPolicy::StretchLeft);
+            map.one_sew(dsplit1, d4, SewPolicy::StretchRight);
+            map.one_sew(dsplit2, d2, SewPolicy::StretchRight);
+        } else {
+            map.one_unsew(d2, UnsewPolicy::DoNothing);
+            map.one_unsew(d4, UnsewPolicy::DoNothing);
+            map.one_sew(d2, dsplit1, SewPolicy::StretchLeft);
+            map.one_sew(d4, dsplit2, SewPolicy::StretchLeft);
+            map.one_sew(dsplit1, d1, SewPolicy::StretchRight);
+            map.one_sew(dsplit2, d3, SewPolicy::StretchRight);
+        }
+    });
+
+    // rebuild faces
+    assert_eq!(map.build_all_faces(), N_SQUARE.pow(2) * 2);
+
     black_box(&mut map);
 }
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    const N_SQUARE: usize = 2_usize.pow(11);
     let map: TwoMap<1> = square_two_map(N_SQUARE);
     let seed: u64 = 9817498146784;
     let rng = SmallRng::seed_from_u64(seed);
