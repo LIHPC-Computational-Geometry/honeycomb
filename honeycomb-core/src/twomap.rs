992685fc41fdec8b9cf3a3fc335ba759a47cda26
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-02-27 10:20:02 +0100

    feat: add map_used_size method

--- honeycomb-core/src/twomap.rs
+++ honeycomb-core/src/twomap.rs
@@ -1388,7 +1388,50 @@ impl<const N_MARKS: usize> TwoMap<N_MARKS> {
         writeln!(file, "others_total, {others_total}").unwrap();
     }
 
-    pub fn map_used_size(&self, filename: &str) {}
+    pub fn map_used_size(&self, filename: &str) {
+        let mut file = File::create(filename).unwrap();
+        writeln!(file, "key, memory (bytes)").unwrap();
+
+        let n_used_darts = self.n_darts - self.free_darts.len();
+        let n_used_vertices = self.n_vertices - self.free_vertices.len();
+
+        // beta
+        let mut beta_total = 0;
+        (0..3).for_each(|beta_id| {
+            let mem = n_used_darts * std::mem::size_of::<DartIdentifier>();
+            writeln!(file, "beta_{beta_id}, {mem}").unwrap();
+            beta_total += mem;
+        });
+        writeln!(file, "beta_total, {beta_total}").unwrap();
+
+        // embed
+        let embed_vertex = n_used_darts * std::mem::size_of::<VertexIdentifier>();
+        let embed_face = n_used_darts * std::mem::size_of::<FaceIdentifier>();
+        let embed_marks = n_used_darts * N_MARKS * std::mem::size_of::<AtomicBool>();
+        let embed_total = embed_vertex + embed_face + embed_marks;
+        writeln!(file, "embed_vertex, {embed_vertex}").unwrap();
+        writeln!(file, "embed_face, {embed_face}").unwrap();
+        writeln!(file, "embed_marks, {embed_marks}").unwrap();
+        writeln!(file, "embed_total, {embed_total}").unwrap();
+
+        // geometry
+        // using 2 * sizeof(f64) bc sizeof(array) always is the size of a pointer
+        let geometry_vertex = n_used_vertices * 2 * std::mem::size_of::<f64>();
+        let geometry_face = self.faces.len() * std::mem::size_of::<Face>();
+        let geometry_total = geometry_vertex + geometry_face;
+        writeln!(file, "geometry_vertex, {geometry_vertex}").unwrap();
+        writeln!(file, "geometry_face, {geometry_face}").unwrap();
+        writeln!(file, "geometry_total, {geometry_total}").unwrap();
+
+        // others
+        let others_freedarts = self.free_darts.len();
+        let others_freevertices = self.free_vertices.len();
+        let others_total =
+            others_freedarts + others_freevertices + 2 * std::mem::size_of::<usize>();
+        writeln!(file, "others_freedarts, {others_freedarts}").unwrap();
+        writeln!(file, "others_freevertices, {others_freevertices}").unwrap();
+        writeln!(file, "others_total, {others_total}").unwrap();
+    }
 }
 
 /// Computes the total size of a given [TwoMap].
