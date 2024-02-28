08f5320590fd4f831fab2ddd52571031b1d5ebda
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-02-28 10:40:33 +0100

    fix: swap the generation function to have correct structure in init benches

--- honeycomb-utils/benches/splitsquaremap/init.rs
+++ honeycomb-utils/benches/splitsquaremap/init.rs
@@ -1,7 +1,7 @@
 //! This benchmarks handle measurements of the initialization speed
 //! of the TwoMap structure in the context of an orthogonal 2D mesh
-//! representation. The exact structure is described in the doc of
-//! the `generation::square_two_map method`.
+//! representation, each cell being split diagonally. The exact
+//! structure is described in the doc of the `generation::square_two_map method`.
 
 // ------ IMPORTS
 
@@ -10,7 +10,7 @@ use criterion::{
     PlotConfiguration, Throughput,
 };
 use honeycomb_core::TwoMap;
-use honeycomb_utils::generation::square_two_map;
+use honeycomb_utils::generation::splitsquare_two_map;
 
 // ------ CONTENT
 
@@ -24,7 +24,7 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         group.throughput(Throughput::Elements(n_square.pow(2) as u64));
         group.bench_with_input(BenchmarkId::new("init", ""), &n_square, |b, n_square| {
             b.iter(|| {
-                let mut map: TwoMap<1> = square_two_map(*n_square);
+                let mut map: TwoMap<1> = splitsquare_two_map(*n_square);
                 black_box(&mut map);
             })
         });
