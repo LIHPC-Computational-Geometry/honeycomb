142221e9972033077a18fd40c5619986d6f82a68
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-03-01 08:45:12 +0100

    feat: add iai benchmarks for reading interfaces

--- honeycomb-utils/benches/core/twomap/reading.rs
+++ honeycomb-utils/benches/core/twomap/reading.rs
@@ -1,7 +1,25 @@
+//! These benchmarks uses iai-callgrind to fetch data from hardware counter
+//! & provide accurate insights into the code behavior independently from
+//! available computing power.
+//!
+//! This file contains benchmarks of key reading methods, classfied into
+//! three groups
+//!
+//! - `bench_read_beta`: benches accesses to beta methods
+//! - `bench_is_free`: benches `is_free` & `is_i_free` methods
+//! - `bench_cell_computation`: benches the `i_cell` method.
+//!
+//! Each benchmark is repeated on TwoMap of different sizes.
+//!
+
+// ------ IMPORTS
+
 use honeycomb_core::{DartIdentifier, TwoMap};
 use honeycomb_utils::generation::square_two_map;
 use iai_callgrind::{library_benchmark, library_benchmark_group, main};
 use std::hint::black_box;
+
+// ------ CONTENT
 
 fn get_map(n_square: usize) -> TwoMap<1> {
     square_two_map::<1>(n_square)
@@ -62,4 +80,63 @@ library_benchmark_group!(
         single_beta_random_darts
 );
 
-main!(library_benchmark_groups = bench_read_beta);
+#[library_benchmark]
+#[bench::small(&get_map(5))]
+#[bench::medium(&get_map(50))]
+#[bench::large(&get_map(500))]
+fn i_free(map: &TwoMap<1>) -> bool {
+    black_box(map.is_i_free::<1>(3))
+}
+
+#[library_benchmark]
+#[bench::small(&get_map(5))]
+#[bench::medium(&get_map(50))]
+#[bench::large(&get_map(500))]
+fn free(map: &TwoMap<1>) -> bool {
+    black_box(map.is_free(3))
+}
+
+library_benchmark_group!(
+    name = bench_is_free;
+    benchmarks =
+        i_free,
+        free,
+);
+
+#[library_benchmark]
+#[bench::small(&get_map(5))]
+#[bench::medium(&get_map(50))]
+#[bench::large(&get_map(500))]
+fn zero_cell(map: &TwoMap<1>) -> Vec<DartIdentifier> {
+    black_box(map.i_cell::<0>(5))
+}
+
+#[library_benchmark]
+#[bench::small(&get_map(5))]
+#[bench::medium(&get_map(50))]
+#[bench::large(&get_map(500))]
+fn one_cell(map: &TwoMap<1>) -> Vec<DartIdentifier> {
+    black_box(map.i_cell::<0>(5))
+}
+
+#[library_benchmark]
+#[bench::small(&get_map(5))]
+#[bench::medium(&get_map(50))]
+#[bench::large(&get_map(500))]
+fn two_cell(map: &TwoMap<1>) -> Vec<DartIdentifier> {
+    black_box(map.i_cell::<2>(5))
+}
+
+library_benchmark_group!(
+    name = bench_cell_computation;
+    benchmarks =
+        zero_cell,
+        one_cell,
+        two_cell,
+);
+
+main!(
+    library_benchmark_groups = bench_read_beta,
+    bench_is_free,
+    bench_cell_computation
+);
