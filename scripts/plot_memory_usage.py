4e845c6b1995a71069989fcaac3e7ae734c17256
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-03-02 10:41:40 +0100

    refactor: move args parsing to dedicated function

--- scripts/plot_memory_usage.py
+++ scripts/plot_memory_usage.py
@@ -2,6 +2,43 @@ import sys
 import matplotlib.pyplot as plt
 import csv
 
+def parseCommandLine():
+    opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
+    
+    if len(args) > 1:
+        print("W: Multiple arguments provided when only one is needed")
+        print("Usage:")
+        print("$ python3 plot_memory_usage.py <CSVFILE> <OPTIONS>")
+
+    filename = args[0]
+
+    overview = False
+    detailed = False
+    all_cats = False
+
+    if ".csv" not in filename:
+        print("W: Specified file may not have the correct format")
+
+    if "--help" in opts or "-h" in opts: # --help / -h -- prints a help message
+        print("Usage:")
+        print("$ python3 plot_memory_usage.py <CSVFILE> <OPTIONS>")
+        print("Available options:")
+        print("  --help       -h  --  prints this message")
+        print("  --overview   -o  --  generates a plot exclusively using total category values")
+        print("  --detailed   -d  --  generates a plot using all values of each category")
+        print("  --all        -a  --  generates a zoomed-in plot for each category")
+        exit(0)
+    
+    if "--overview" in opts or "-o" in opts:
+        overview = True
+    if "--detailed" in opts or "-d" in opts:
+        detailed = True
+    if "--all" in opts or "-a" in opts:
+        all_cats = True
+    
+    return (filename, overview, detailed, all_cats)
+
 def parseDataFromFile(filename: str):
     beta = []
     embed = []
@@ -36,41 +73,10 @@ def parseDataFromFile(filename: str):
     return (beta, embed, geometry, others, totals)
 
 def run():
-    opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
-    args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
+    filename, overview, detailed, all_cats = parseCommandLine()
+
+    beta, embed, geometry, others, totals = parseDataFromFile(filename)
     
-    if len(args) > 1:
-        print("W: Multiple arguments provided when only one is needed")
-        print("Usage:")
-        print("$ python3 plot_memory_usage.py <CSVFILE> <OPTIONS>")
-
-    filename = args[0]
-
-    overview = False
-    detailed = False
-    all = False
-
-    if ".csv" not in filename:
-        print("W: Specified file may not have the correct format")
-
-    if "--help" in opts or "-h" in opts: # --help / -h -- prints a help message
-        print("Usage:")
-        print("$ python3 plot_memory_usage.py <CSVFILE> <OPTIONS>")
-        print("Available options:")
-        print("  --help       -h  --  prints this message")
-        print("  --overview   -o  --  generates a plot exclusively using total category values")
-        print("  --detailed   -d  --  generates a plot using all values of each category")
-        print("  --all        -a  --  generates a zoomed-in plot for each category")
-        return
-    else:
-        if "--overview" in opts or "-o" in opts:
-            overview = True
-        if "--detailed" in opts or "-d" in opts:
-            detailed = True
-        if "--all" in opts or "-a" in opts:
-            all = True
-    
-    beta, embed, geometry, others, totals = parseDataFromFile(args[0])
     print(totals)
 
 
