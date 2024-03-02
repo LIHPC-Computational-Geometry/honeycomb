7c6839a992b8f05dbde68acbf6aa59b7a68d6619
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-03-02 10:29:53 +0100

    feat: add options & help message to python script

--- scripts/plot_memory_usage.py
+++ scripts/plot_memory_usage.py
@@ -38,7 +38,38 @@ def parseDataFromFile(filename: str):
 def run():
     opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
     args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
+    
+    if len(args) > 1:
+        print("W: Multiple arguments provided when only one is needed")
+        print("Usage:")
+        print("$ python3 plot_memory_usage.py <CSVFILE> <OPTIONS>")
 
+    filename = args[0]
+
+    overview = False
+    detailed = False
+    all = False
+
+    if ".csv" not in filename:
+        print("W: Specified file may not have the correct format")
+
+    if "--help" in opts or "-h" in opts: # --help / -h -- prints a help message
+        print("Usage:")
+        print("$ python3 plot_memory_usage.py <CSVFILE> <OPTIONS>")
+        print("Available options:")
+        print("  --help       -h  --  prints this message")
+        print("  --overview   -o  --  generates a plot exclusively using total category values")
+        print("  --detailed   -d  --  generates a plot using all values of each category")
+        print("  --all        -a  --  generates a zoomed-in plot for each category")
+        return
+    else:
+        if "--overview" in opts or "-o" in opts:
+            overview = True
+        if "--detailed" in opts or "-d" in opts:
+            detailed = True
+        if "--all" in opts or "-a" in opts:
+            all = True
+    
     beta, embed, geometry, others, totals = parseDataFromFile(args[0])
     print(totals)
 
