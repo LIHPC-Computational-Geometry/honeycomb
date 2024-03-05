97f709865abdd0f1a1f24237553ae7647cb52d66
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-03-05 11:20:22 +0100

    feat: add file for custome vec type

---
+++ honeycomb-core/src/coords.rs
@@ -0,0 +1,30 @@
+//! Module short description
+//!
+//! Should you interact with this module directly?
+//!
+//! Content description if needed
+
+// ------ MODULE DECLARATIONS
+
+// ------ IMPORTS
+
+// ------ CONTENT
+
+pub type FloatType = f64;
+
+pub struct Coords2 {
+    pub x: FloatType,
+    pub y: FloatType,
+}
+
+// ------ TESTS
+
+#[cfg(test)]
+mod tests {
+    //use super::*;
+
+    #[test]
+    fn some_test() {
+        assert_eq!(1, 1);
+    }
+}
