43e6dc9ad7c0b0ee414066de3c20ac36ad7d4556
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-03-13 14:34:22 +0100

    doc: add crate-level documentation to honeycomb-render

--- honeycomb-render/src/lib.rs
+++ honeycomb-render/src/lib.rs
@@ -1,8 +1,24 @@
-//! Module short description
+//! # honeycomb-render
 //!
-//! Should you interact with this module directly?
+//! This crate implements a runner that can be used to display
+//! combinatorial maps.
 //!
-//! Content description if needed
+//! It currently only supports 2D maps as the core library only
+//! implements these (as [TwoMap])
+//!
+//! ## Quickstart
+//!
+//! The crate provides the following example:
+//!
+//! - `render_default_no_aa` -- Render a hardcoded arrow without anti-aliasing.
+//! - `render_default_smaa1x` -- Render a hardcoded arrow with anti-aliasing.
+//! - `render_splitsquaremap` -- Render a map generated using functions provided by
+//!   the **honeycomb-utils** crate.
+//! - `render_squaremap` -- Render a map generated using functions provided by the
+//!   **honeycomb-utils** crate.
+
+#[cfg(doc)]
+use honeycomb_core::TwoMap;
 
 // ------ MODULE DECLARATIONS
 
