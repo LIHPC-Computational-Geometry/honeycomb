cb6fcf68e9fe84b981c18e193e33e236ce5556ea
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-03-13 15:08:36 +0100

    feat: add keleton structures

--- honeycomb-core/src/orbits.rs
+++ honeycomb-core/src/orbits.rs
@@ -1,5 +1,22 @@
-use crate::{CoordsFloat, TwoMap};
+use crate::{CoordsFloat, DartIdentifier, TwoMap};
+use std::collections::BTreeSet;
+
+struct OrbitCore<'a, const N_MARKS: usize, T: CoordsFloat> {
+    map_handle: &'a TwoMap<N_MARKS, T>,
+    beta_slice: &'a [u8],
+    visited: BTreeSet<DartIdentifier>,
+    pending: Vec<DartIdentifier>,
+}
 
 pub struct Orbit<'a, const N_MARKS: usize, T: CoordsFloat> {
-    map_handle: &'a TwoMap<N_MARKS, T>,
+    ocore: OrbitCore<'a, N_MARKS, T>,
+    current_dart: DartIdentifier,
+}
+
+impl<'a, const N_MARKS: usize, T: CoordsFloat> Iterator for Orbit<'a, N_MARKS, T> {
+    type Item = DartIdentifier;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!()
+    }
 }
