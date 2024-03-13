81c54692b0bd363613292c6d112527811104f01f
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-03-13 15:44:16 +0100

    feat: implement Iterator for Orbit structure using a BFS algorithm

--- honeycomb-core/src/orbits.rs
+++ honeycomb-core/src/orbits.rs
@@ -11,23 +11,34 @@ use std::collections::{BTreeSet, VecDeque};
 
 // ------ CONTENT
 
-struct OrbitCore<'a, const N_MARKS: usize, T: CoordsFloat> {
-    pub map_handle: &'a TwoMap<N_MARKS, T>,
-    pub beta_slice: &'a [u8],
-    pub visited: BTreeSet<DartIdentifier>,
-    pub pending: VecDeque<DartIdentifier>,
-}
-
 pub struct Orbit<'a, const N_MARKS: usize, T: CoordsFloat> {
-    ocore: OrbitCore<'a, N_MARKS, T>,
-    current_dart: DartIdentifier,
+    map_handle: &'a TwoMap<N_MARKS, T>,
+    pub beta_slice: &'a [u8],
+    marked: BTreeSet<DartIdentifier>,
+    pending: VecDeque<DartIdentifier>,
 }
 
 impl<'a, const N_MARKS: usize, T: CoordsFloat> Iterator for Orbit<'a, N_MARKS, T> {
     type Item = DartIdentifier;
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        if let Some(d) = self.pending.pop_front() {
+            self.beta_slice.iter().for_each(|beta_id| {
+                let image = self.map_handle.beta_bis(*beta_id, d);
+                if self.marked.insert(image) {
+                    // if true, we did not see this dart yet
+                    // i.e. we need to visit it later
+                    self.pending.push_back(image);
+                }
+            });
+
+            Some(d)
+        } else {
+            // this makes the structure reusable
+            self.marked.clear();
+            self.pending.clear();
+            None
+        }
     }
 }
 
