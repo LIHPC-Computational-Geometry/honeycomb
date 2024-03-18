80debb82bcbde9b24e6f57b1825a08475fc2190e
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-03-18 15:28:42 +0100

    feat: add usual methods for Vector2

--- honeycomb-core/src/vector.rs
+++ honeycomb-core/src/vector.rs
@@ -6,12 +6,117 @@
 
 // ------ IMPORTS
 
-use crate::{Coords2, CoordsFloat};
+use crate::{Coords2, CoordsError, CoordsFloat};
 
 // ------ CONTENT
 
 pub struct Vector2<T: CoordsFloat> {
     inner: Coords2<T>,
+}
+
+impl<T: CoordsFloat> Vector2<T> {
+    /// Base vector
+    ///
+    /// # Return
+    ///
+    /// Return a unit vector along the `x` axis.
+    ///
+    pub fn unit_x() -> Self {
+        Self {
+            inner: Coords2::unit_x(),
+        }
+    }
+
+    /// Base vector
+    ///
+    /// # Return
+    ///
+    /// Return a unit vector along the `y` axis.
+    ///
+    pub fn unit_y() -> Self {
+        Self {
+            inner: Coords2::unit_y(),
+        }
+    }
+
+    pub fn into_inner(self) -> Coords2<T> {
+        self.inner
+    }
+
+    pub fn x(&self) -> T {
+        self.inner.x
+    }
+
+    pub fn y(&self) -> T {
+        self.inner.y
+    }
+
+    /// Compute the norm of `self`.
+    ///
+    /// # Return
+    ///
+    /// Return the norm. Its type is the same as the one used for internal
+    /// representation.
+    ///
+    /// # Example
+    ///
+    /// See [Coords2] example.
+    ///
+    pub fn norm(&self) -> T {
+        self.inner.norm()
+    }
+
+    /// Compute the direction of `self` as a unit vector.
+    ///
+    /// # Return
+    ///
+    /// Return a [Vector2] indicating the direction of `self`. The norm of the returned
+    /// struct is equal to one.
+    ///
+    /// # Example
+    ///
+    /// See [Coords2] example.
+    ///
+    pub fn unit_dir(&self) -> Result<Vector2<T>, CoordsError> {
+        self.inner
+            .unit_dir()
+            .map(|coords2: Coords2<T>| Self { inner: coords2 })
+    }
+
+    /// Compute the direction of the normal vector to `self`.
+    ///
+    /// # Return
+    ///
+    /// Return a [Vector2] indicating the direction of the normal to `self`. The norm of the
+    /// returned struct is equal to one.
+    ///
+    /// # Example
+    ///
+    /// See [Coords2] example.
+    ///
+    pub fn normal_dir(&self) -> Vector2<T> {
+        Self {
+            inner: self.inner.normal_dir(),
+        }
+    }
+
+    /// Compute the dot product between two vectors
+    ///
+    /// # Arguments
+    ///
+    /// - `other: &Vector2` -- reference to the second vector.
+    ///
+    /// # Return
+    ///
+    /// Return the dot product between `self` and `other`.
+    ///
+    /// # Example
+    ///
+    /// See [Coords2] example.
+    ///
+    pub fn dot(&self, other: &Vector2<T>) -> T {
+        self.inner.dot(&other.inner)
+    }
 }
 
 // Building traits
