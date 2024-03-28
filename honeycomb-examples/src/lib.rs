bc8e6063fc83378be674159d7600b88bc27c9e14
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-03-28 12:05:57 +0100

    doc: add a crate-level documentation for honeycomb-examples

---
+++ honeycomb-examples/src/lib.rs
@@ -0,0 +1,19 @@
+//! # honeycomb-examples
+//!
+//! This crate contains all examples of the project.
+//!
+//! ## Available examples
+//!
+//! ### Utilities
+//!
+//! - `memory_usage` -- Save the memory usage of a given map as three *csv* files. These files
+//!   can be used to generate charts using the `memory_usage.py` script.
+//!
+//! ### Rendering
+//!
+//! - `render_default_no_aa` -- Render a hardcoded arrow without anti-aliasing.
+//! - `render_default_smaa1x` -- Render a hardcoded arrow with anti-aliasing.
+//! - `render_splitsquaremap` -- Render a map generated using functions provided by
+//!   the **honeycomb-utils** crate.
+//! - `render_squaremap` -- Render a map generated using functions provided by the
+//!   **honeycomb-utils** crate.
