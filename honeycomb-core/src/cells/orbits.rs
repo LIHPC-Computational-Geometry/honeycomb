0a377235f92f685c40932e8570a73c9fba2d3ade
Isaïe <isaie.muron@cea.fr>
2024-04-08 08:11:01 +0200

    refactor: restructure core crate source code (#27)
    * feat: create new module (cells)
    
    * feat: create new module (cells::identifiers)
    
    * refactor: move orbits module inside cells mod
    
    * refactor: move all ID type definitions to identifiers module
    
    also, add NULL definitions for all cell types
    
    * feat: create spatial_repr module
    
    also, move in definitions of FloatType & CoordsFloat
    
    * refactor: move coords, vector, vertex modules into spatial_repr
    
    * refactor: remove dummy tests
    
    * refactor: update & format imports
    
    * chore: update changelog

--- honeycomb-core/src/cells/orbits.rs
+++ honeycomb-core/src/cells/orbits.rs
@@ -6,6 +6,7 @@
 // ------ IMPORTS
 
 use crate::{CMap2, CoordsFloat, DartIdentifier, NULL_DART_ID};
+
 use num::Zero;
 use std::collections::{BTreeSet, VecDeque};
 
@@ -178,8 +179,7 @@ impl<'a, T: CoordsFloat> Iterator for Orbit2<'a, T> {
 
 #[cfg(test)]
 mod tests {
-    use crate::orbits::OrbitPolicy;
-    use crate::{CMap2, DartIdentifier, FloatType, Orbit2};
+    use crate::{CMap2, DartIdentifier, FloatType, Orbit2, OrbitPolicy};
 
     fn simple_map() -> CMap2<FloatType> {
         let mut map: CMap2<FloatType> = CMap2::new(6, 4);
