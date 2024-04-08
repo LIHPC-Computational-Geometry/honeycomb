d9725a269a6c841e5198cc10bca68cba0434005d
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-04-08 08:21:11 +0200

    refactor: move utils crate content to the new core module

--- honeycomb-utils/src/lib.rs
+++ honeycomb-utils/src/lib.rs
@@ -3,19 +3,3 @@
 //! This crate implements basic utilities used for tests & benchmarking
 //! of the structures & methods implemented in **honeycomb_core**.
 //!
-
-// ------ MODULE DECLARATIONS
-
-pub mod generation;
-
-// ------ TESTS
-
-#[cfg(test)]
-mod tests {
-    //use super::*;
-
-    #[test]
-    fn some_test() {
-        assert_eq!(1, 1);
-    }
-}
