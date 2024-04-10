7e10be6aef48192eb47bc835f6a2cab32a4f9047
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-04-10 15:07:12 +0200

    feat: create cells::attribute_collections module

---
+++ honeycomb-core/src/cells/attribute_collections.rs
@@ -0,0 +1,21 @@
+//! Module short description
+//!
+//! Should you interact with this module directly?
+//!
+//! Content description if needed
+
+// ------ IMPORTS
+
+// ------ CONTENT
+
+// ------ TESTS
+
+#[cfg(test)]
+mod tests {
+    //use super::*;
+
+    #[test]
+    fn some_test() {
+        assert_eq!(1, 1);
+    }
+}
