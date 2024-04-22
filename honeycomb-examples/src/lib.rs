055eb59d8417b0cf8a7c2b42b5f89b5d26ff6b54
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-04-22 15:12:09 +0200

    doc: update the lib.rs of honeycomb-examples

--- honeycomb-examples/src/lib.rs
+++ honeycomb-examples/src/lib.rs
@@ -13,7 +13,13 @@
 //!
 //! - `render_default_no_aa` -- Render a hardcoded arrow without anti-aliasing.
 //! - `render_default_smaa1x` -- Render a hardcoded arrow with anti-aliasing.
-//! - `render_splitsquaremap` -- Render a map generated using functions provided by
-//!   the **honeycomb-benches** crate.
-//! - `render_squaremap` -- Render a map generated using functions provided by the
-//!   **honeycomb-benches** crate.
+//! - `render_splitsquaremap` -- Render a map generated using functions defined in the `utils`
+//!   module of the core crate
+//! - `render_squaremap` -- Render a map generated using functions  defined in the `utils` module
+//!   of the core crate.
+//! - `render_squaremap_shift` -- Render a map computed by the `squaremap-shift` benchmark. Inner
+//!   vertices are shifted by a random offset value.
+//! - `render_squaremap_split_diff` -- Render a map computed by the `squaremap-splitquads`
+//!   benchmark. All quads are split diagonally, which diagonal chosen at random.
+//! - `render_squaremap_split_some` -- Render a map computed by the `squaremap-splitquads`
+//!   benchmark. Only some quads are split diagonally, chosen at random.
