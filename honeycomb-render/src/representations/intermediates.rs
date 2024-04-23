1dfaec287704b2b608d95b5bbf62811d0235e6aa
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-04-23 07:57:34 +0200

    refactor: add 0/1 vertex specific cases in face IR constructor

--- honeycomb-render/src/representations/intermediates.rs
+++ honeycomb-render/src/representations/intermediates.rs
@@ -1,4 +1,4 @@
-use honeycomb_core::{CoordsFloat, Vertex2};
+use honeycomb_core::{Coords2, CoordsFloat, Vertex2};
 
 pub enum Entity {
     Dart,
@@ -15,13 +15,17 @@ impl<T: CoordsFloat> IntermediateFace<T> {
     pub fn new<I: Iterator<Item = Vertex2<T>>>(it: I) -> Self {
         let tmp: Vec<Vertex2<T>> = it.collect();
         let n_vertices = tmp.len();
-        let center = Vertex2::from(
-            tmp.iter()
-                .map(|vertex2: &Vertex2<T>| vertex2.into_inner())
-                .reduce(|coords1, coords2| coords1 + coords2)
-                .unwrap()
-                / T::from(n_vertices).unwrap(),
-        );
+        let center = match n_vertices {
+            // with 0 or 1 vertex, there won't be much to render, we can throw in a dummy value
+            0 | 1 => Vertex2::from((T::zero(), T::zero())),
+            // otherwise, compute the average of all vertices to get the center of the cell
+            _ => Vertex2::from(
+                tmp.iter()
+                    .map(|vertex2: &Vertex2<T>| vertex2.into_inner())
+                    .sum::<Coords2<T>>()
+                    / T::from(n_vertices).unwrap(),
+            ),
+        };
         Self {
             n_vertices,
             vertices: tmp,
