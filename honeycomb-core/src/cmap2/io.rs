76533e50f81357fd273d7f22353c3f962f65f17c
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-05-07 10:29:51 +0200

    feat: complete vertex list building

--- honeycomb-core/src/cmap2/io.rs
+++ honeycomb-core/src/cmap2/io.rs
@@ -13,14 +13,34 @@ use vtkio::IOBuffer;
 
 // ------ CONTENT
 
+// --- macros
+
+macro_rules! build_vertices {
+    ($v: ident) => {{
+        assert!(
+            ($v.len() % 3).is_zero(),
+            "failed to fetch vertex data - the point list contains an incomplete tuple"
+        );
+        $v.chunks_exact(3)
+            .map(|slice| {
+                // WE IGNORE Z values
+                let &[x, y, _] = slice else { panic!() };
+                Vertex2::from((T::from(x).unwrap(), T::from(y).unwrap()))
+            })
+            .collect()
+    }};
+}
+
+// --- implementations
+
 impl<T: CoordsFloat> From<Vtk> for CMap2<T> {
     fn from(value: Vtk) -> Self {
         match value.data {
             DataSet::ImageData { .. }
-                | DataSet::StructuredGrid { .. }
-                | DataSet::RectilinearGrid { .. }
-                | DataSet::PolyData { .. }
-                | DataSet::Field { .. } => {}
+            | DataSet::StructuredGrid { .. }
+            | DataSet::RectilinearGrid { .. }
+            | DataSet::PolyData { .. }
+            | DataSet::Field { .. } => {}
             DataSet::UnstructuredGrid { pieces, .. } => pieces.iter().for_each(|piece| {
                 // assume inline data
                 let tmp = piece
@@ -29,21 +49,10 @@ impl<T: CoordsFloat> From<Vtk> for CMap2<T> {
 
                 // build vertex list
                 // since we're expecting coordinates, we'll assume floating type
+                // we're also converting directly to our vertex type since we're building a 2-map
                 let vertices: Vec<Vertex2<T>> = match tmp.points {
-                    IOBuffer::F64(v) => {
-                        assert!(
-                            (v.len() % 3).is_zero(),
-                            "failed to fetch vertex data - the point list contains an incomplete tuple"
-                        );
-                        todo!()
-                    }
-                    IOBuffer::F32(v) => {
-                        assert!(
-                            (v.len() % 3).is_zero(),
-                            "failed to fetch vertex data - the point list contains an incomplete tuple"
-                        );
-                        todo!()
-                    }
+                    IOBuffer::F64(v) => build_vertices!(v),
+                    IOBuffer::F32(v) => build_vertices!(v),
                     _ => unimplemented!(),
                 };
             }),
