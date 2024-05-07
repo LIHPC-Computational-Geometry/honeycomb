a8b119a73e397ced201a1e349352a5dcf30a22f5
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-05-07 12:01:09 +0200

    feat: add basic cell processing
    tests would be nice

--- honeycomb-core/src/cmap2/io.rs
+++ honeycomb-core/src/cmap2/io.rs
@@ -6,9 +6,9 @@
 
 // ------ IMPORTS
 
-use crate::{CMap2, CoordsFloat, Vertex2};
+use crate::{CMap2, CoordsFloat, Vertex2, VertexIdentifier};
 use num::Zero;
-use vtkio::model::{DataSet, Vtk};
+use vtkio::model::{CellType, Cells, DataSet, VertexNumbers, Vtk};
 use vtkio::IOBuffer;
 
 // ------ CONTENT
@@ -19,7 +19,7 @@ macro_rules! build_vertices {
     ($v: ident) => {{
         assert!(
             ($v.len() % 3).is_zero(),
-            "failed to fetch vertex data - the point list contains an incomplete tuple"
+            "failed to build vertices list - the point list contains an incomplete tuple"
         );
         $v.chunks_exact(3)
             .map(|slice| {
@@ -55,6 +55,54 @@ impl<T: CoordsFloat> From<Vtk> for CMap2<T> {
                     IOBuffer::F32(v) => build_vertices!(v),
                     _ => unimplemented!(),
                 };
+
+                let Cells { cell_verts, types } = tmp.cells;
+                match cell_verts {
+                    VertexNumbers::Legacy {
+                        num_cells,
+                        vertices,
+                    } => {
+                        // check basic stuff
+                        assert_eq!(
+                            num_cells as usize,
+                            types.len(),
+                            "failed to build cells - inconsistent number of cell between CELLS and CELL_TYPES"
+                        );
+
+                        // build a collection of vertex lists correspondign of each cell
+                        let mut cell_components: Vec<Vec<VertexIdentifier>> = Vec::new();
+                        let mut take_next = 0;
+                        vertices.iter().for_each(|vertex_id| if take_next.is_zero() {
+                            // making it usize since it's a counter
+                            take_next = *vertex_id as usize;
+                            cell_components.push(Vec::with_capacity(take_next));
+                        } else {
+                            cell_components.last_mut().unwrap().push(*vertex_id as VertexIdentifier);
+                            take_next -= 1;
+                        });
+                        assert_eq!(num_cells as usize, cell_components.len());
+
+                        types.iter().for_each(|cell_type| match cell_type {
+                            CellType::Vertex => {}
+                            CellType::PolyVertex => {}
+                            CellType::Line => {}
+                            CellType::PolyLine => {}
+                            CellType::Triangle => {}
+                            CellType::TriangleStrip => {}
+                            CellType::Polygon => {}
+                            CellType::Pixel => unimplemented!(
+                                "failed to build cell - `Pixel` cell type is not supported because of orientation requirements"
+                            ),
+                            CellType::Quad => {}
+                            c => unimplemented!(
+                                "failed to build cell - {c:#?} is not supported in 2-maps"
+                            ),
+                        });
+                    }
+                    VertexNumbers::XML { .. } => {
+                        todo!()
+                    }
+                }
             }),
         }
         todo!()
