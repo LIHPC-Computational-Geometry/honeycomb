f35e43641fbab63fcb1853142cb287546d4a7095
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-05-07 08:33:50 +0200

    feat: add skeleton structure for vertex parsing

--- honeycomb-core/src/cmap2/io.rs
+++ honeycomb-core/src/cmap2/io.rs
@@ -6,19 +6,53 @@
 
 // ------ IMPORTS
 
-use vtkio::xml::VTKFile;
-
-use crate::{CMap2, CoordsFloat};
+use crate::{CMap2, CoordsFloat, Vertex2};
+use num::Zero;
+use vtkio::model::{DataSet, Vtk};
+use vtkio::IOBuffer;
 
 // ------ CONTENT
 
-impl<T: CoordsFloat> From<VTKFile> for CMap2<T> {
-    fn from(value: VTKFile) -> Self {
+impl<T: CoordsFloat> From<Vtk> for CMap2<T> {
+    fn from(value: Vtk) -> Self {
+        match value.data {
+            DataSet::ImageData { .. }
+                | DataSet::StructuredGrid { .. }
+                | DataSet::RectilinearGrid { .. }
+                | DataSet::PolyData { .. }
+                | DataSet::Field { .. } => {}
+            DataSet::UnstructuredGrid { pieces, .. } => pieces.iter().for_each(|piece| {
+                // assume inline data
+                let tmp = piece
+                    .load_piece_data(None)
+                    .expect("failed to load piece data");
+
+                // build vertex list
+                // since we're expecting coordinates, we'll assume floating type
+                let vertices: Vec<Vertex2<T>> = match tmp.points {
+                    IOBuffer::F64(v) => {
+                        assert!(
+                            (v.len() % 3).is_zero(),
+                            "failed to fetch vertex data - the point list contains an incomplete tuple"
+                        );
+                        todo!()
+                    }
+                    IOBuffer::F32(v) => {
+                        assert!(
+                            (v.len() % 3).is_zero(),
+                            "failed to fetch vertex data - the point list contains an incomplete tuple"
+                        );
+                        todo!()
+                    }
+                    _ => unimplemented!(),
+                };
+            }),
+        }
         todo!()
     }
 }
 
-impl<T: CoordsFloat> From<CMap2<T>> for VTKFile {
+impl<T: CoordsFloat> From<CMap2<T>> for Vtk {
     fn from(value: CMap2<T>) -> Self {
         todo!()
     }
