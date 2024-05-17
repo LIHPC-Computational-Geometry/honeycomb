7dfedceb9d5b0b47da5f07b8be9dea775ea538c5
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-05-17 10:39:16 +0200

    add a correct almsot_equal function for fp scalars

--- honeycomb-core/src/spatial_repr/tests.rs
+++ honeycomb-core/src/spatial_repr/tests.rs
@@ -2,10 +2,34 @@
 
 // ------ CONTENT
 
+use crate::CoordsFloat;
+
 // --- common
+
+// ref: https://floating-point-gui.de/errors/comparison/
+fn almost_equal<T: CoordsFloat>(a: T, b: T) -> bool {
+    let abs_diff = (a - b).abs();
+    let abs_sum = a.abs() + b.abs();
+    if a == b {
+        // early return
+        true
+    } else if a.is_zero() | b.is_zero() | (abs_sum < T::min_positive_value()) {
+        // close to zero
+        abs_diff < (T::epsilon() * T::min_positive_value())
+    } else {
+        // regular case
+        abs_diff / (abs_sum).min(T::max_value()) < T::epsilon()
+    }
+}
 
 // --- coords
 
+mod coords {}
+
 // --- vector
 
+mod vector {}
+
 // --- vertex
+
+mod vertex {}
