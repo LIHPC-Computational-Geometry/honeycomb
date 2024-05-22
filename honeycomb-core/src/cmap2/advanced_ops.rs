85be942b2ec147047155e3b1791497a60d88c100
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-05-22 09:53:59 +0200

    add documentation for the split_edge method

--- honeycomb-core/src/cmap2/advanced_ops.rs
+++ honeycomb-core/src/cmap2/advanced_ops.rs
@@ -10,6 +10,18 @@ use crate::{CMap2, CoordsFloat, EdgeIdentifier};
 // ------ CONTENT
 
 impl<T: CoordsFloat> CMap2<T> {
+    /// Split an edge into to segments.
+    ///
+    /// <div class="warning">
+    /// This implementation is 2D specific.
+    /// </div>
+    ///
+    /// This method takes all darts of an edge and rebuild connections in order to create a new
+    /// point on this edge. The position of the point default to the midway point, but it can
+    /// optionally be specified.
+    ///
+    /// In order to minimize editing of embedded data, the original darts are kept to their
+    /// original vertices while the new darts are used to model the new point.
     pub fn split_edge(&mut self, edge_id: EdgeIdentifier) {
         todo!()
     }
