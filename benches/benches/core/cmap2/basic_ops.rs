c1287607adf7c51e31dc329f2051dbcd0ac740ad
Isaïe <isaie.muron@cea.fr>
2024-05-27 10:36:28 +0200

    refactor(benches): update and fix benchmarks  (#84)
    * update iai benchmarks size range
    
    * split editing benches into constructors & basic_ops
    
    * update basic_ops benches
    
    * remove reading.rs & relocate useful benches from it
    
    * update link and sew benches
    
    * update criterion bench sizes
    
    * update benches documentation

---
+++ benches/benches/core/cmap2/basic_ops.rs
@@ -0,0 +1,179 @@
+//! These benchmarks uses iai-callgrind to fetch data from hardware counter
+//! & provide accurate insights into the code behavior independently from
+//! available computing power.
+//!
+//! This file contains benchmarks of basic operations, classified into three groups:
+//!
+//! - `bench_darts`: benches dart-related methods.
+//! - `bench_vertices`: benches vertex-related methods.
+//!
+//! Each benchmark is repeated on CMap2 of different sizes.
+
+// ------ IMPORTS
+
+use honeycomb_benches::FloatType;
+use honeycomb_core::{utils::GridBuilder, CMap2, CMapError, DartIdentifier, Vertex2};
+use iai_callgrind::{
+    library_benchmark, library_benchmark_group, main, FlamegraphConfig, LibraryBenchmarkConfig,
+};
+use std::hint::black_box;
+
+// ------ CONTENT
+
+// --- common
+
+fn get_map(n_square: usize) -> CMap2<FloatType> {
+    GridBuilder::unit_squares(n_square).build2().unwrap()
+}
+
+fn get_sparse_map(n_square: usize) -> CMap2<FloatType> {
+    let mut map = GridBuilder::unit_squares(n_square).build2().unwrap();
+    map.set_betas(5, [0; 3]); // free dart 5
+    map.remove_free_dart(5);
+    // because of the way we built the map in the square_cmap2 function & the ID computation
+    // policy, we can safely remove a vertex we know is defined
+    assert_eq!(map.remove_vertex(1).unwrap(), Vertex2::from((0.0, 0.0)));
+    map
+}
+
+fn get_empty_map(n_squares: usize) -> (CMap2<FloatType>, usize) {
+    (CMap2::new(0), n_squares.pow(2) * 4)
+}
+
+// --- dart group
+
+#[library_benchmark]
+#[bench::small(&mut get_map(16))]
+#[bench::medium(&mut get_map(64))]
+#[bench::large(&mut get_map(256))]
+fn add_single_dart(map: &mut CMap2<FloatType>) -> DartIdentifier {
+    black_box(map.add_free_dart())
+}
+
+#[library_benchmark]
+#[bench::small(&mut get_map(16))]
+#[bench::medium(&mut get_map(64))]
+#[bench::large(&mut get_map(256))]
+fn add_ten_darts(map: &mut CMap2<FloatType>) -> DartIdentifier {
+    black_box(map.add_free_darts(10))
+}
+
+#[library_benchmark]
+#[benches::with_setup(args = [16, 32, 64, 128, 256, 512], setup = get_empty_map)]
+fn add_many_darts((mut map, n_darts): (CMap2<FloatType>, usize)) -> DartIdentifier {
+    black_box(map.add_free_darts(n_darts))
+}
+
+#[library_benchmark]
+#[bench::small(&mut get_sparse_map(16))]
+#[bench::medium(&mut get_sparse_map(64))]
+#[bench::large(&mut get_sparse_map(256))]
+fn insert_dart(map: &mut CMap2<FloatType>) -> DartIdentifier {
+    black_box(map.insert_free_dart())
+}
+
+#[library_benchmark]
+#[bench::small(&mut get_map(16))]
+#[bench::medium(&mut get_map(64))]
+#[bench::large(&mut get_map(256))]
+fn insert_dart_full(map: &mut CMap2<FloatType>) -> DartIdentifier {
+    black_box(map.insert_free_dart())
+}
+
+#[library_benchmark]
+#[bench::small(&mut CMap2::new(16_usize.pow(2) * 4))]
+#[bench::medium(&mut CMap2::new(64_usize.pow(2) * 4))]
+#[bench::large(&mut CMap2::new(256_usize.pow(2) * 4))]
+fn remove_dart(map: &mut CMap2<FloatType>) {
+    map.remove_free_dart(5);
+    black_box(map);
+}
+
+library_benchmark_group!(
+    name = bench_darts;
+    benchmarks =
+        add_single_dart,
+        add_ten_darts,
+        add_many_darts,
+        insert_dart,
+        insert_dart_full,
+);
+
+// --- vertex group
+
+#[library_benchmark]
+#[bench::small(&mut get_map(16))]
+#[bench::medium(&mut get_map(64))]
+#[bench::large(&mut get_map(256))]
+fn read_vertex(map: &mut CMap2<FloatType>) -> Result<Vertex2<FloatType>, CMapError> {
+    black_box(map.vertex(1))
+}
+
+#[library_benchmark]
+#[bench::small(&mut get_sparse_map(16))]
+#[bench::medium(&mut get_sparse_map(64))]
+#[bench::large(&mut get_sparse_map(256))]
+fn read_missing_vertex(map: &mut CMap2<FloatType>) -> Result<Vertex2<FloatType>, CMapError> {
+    black_box(map.vertex(1))
+}
+
+#[library_benchmark]
+#[bench::small(&mut get_sparse_map(16))]
+#[bench::medium(&mut get_sparse_map(64))]
+#[bench::large(&mut get_sparse_map(256))]
+fn insert_vertex(map: &mut CMap2<FloatType>) {
+    map.insert_vertex(1, (0.0, 0.0));
+    black_box(map);
+}
+
+#[library_benchmark]
+#[bench::small(&mut get_map(16))]
+#[bench::medium(&mut get_map(64))]
+#[bench::large(&mut get_map(256))]
+fn replace_vertex(map: &mut CMap2<FloatType>) -> Result<Vertex2<FloatType>, CMapError> {
+    black_box(map.replace_vertex(1, (0.0, 0.0)))
+}
+
+#[library_benchmark]
+#[bench::small(&mut get_sparse_map(16))]
+#[bench::medium(&mut get_sparse_map(64))]
+#[bench::large(&mut get_sparse_map(256))]
+fn set_vertex(map: &mut CMap2<FloatType>) -> Result<Vertex2<FloatType>, CMapError> {
+    black_box(map.replace_vertex(1, (0.0, 0.0)))
+}
+
+#[library_benchmark]
+#[bench::small(&mut get_map(16))]
+#[bench::medium(&mut get_map(64))]
+#[bench::large(&mut get_map(256))]
+fn remove_vertex(map: &mut CMap2<FloatType>) -> Result<Vertex2<FloatType>, CMapError> {
+    black_box(map.remove_vertex(1))
+}
+
+#[library_benchmark]
+#[bench::small(&mut get_sparse_map(16))]
+#[bench::medium(&mut get_sparse_map(64))]
+#[bench::large(&mut get_sparse_map(256))]
+fn remove_missing_vertex(map: &mut CMap2<FloatType>) -> Result<Vertex2<FloatType>, CMapError> {
+    black_box(map.remove_vertex(1))
+}
+
+library_benchmark_group!(
+    name = bench_vertices;
+    benchmarks =
+        read_vertex,
+        read_missing_vertex,
+        insert_vertex,
+        replace_vertex,
+        set_vertex,
+        remove_vertex,
+        remove_missing_vertex,
+);
+
+// --- main
+
+main!(
+    config = LibraryBenchmarkConfig::default().flamegraph(FlamegraphConfig::default());
+    library_benchmark_groups = bench_darts,
+    bench_vertices,
+);
---
+++ benches/benches/core/cmap2/constructors.rs
@@ -0,0 +1,135 @@
+//! These benchmarks uses iai-callgrind to fetch data from hardware counter
+//! & provide accurate insights into the code behavior independently from
+//! available computing power.
+//!
+//! This file contains benchmarks of key building methods and constructors,
+//! classified into two groups:
+//!
+//! - `bench_constructors`: benches constructors functions.
+//! - `bench_fetches`: benches insertion methods (both behaviors).
+//! - `bench_icells`: benches the i-cell method
+//!
+//! Each benchmark is repeated on CMap2 of different sizes.
+
+// ------ IMPORTS
+
+use honeycomb_benches::FloatType;
+use honeycomb_core::{utils::GridBuilder, CMap2};
+use iai_callgrind::{
+    library_benchmark, library_benchmark_group, main, FlamegraphConfig, LibraryBenchmarkConfig,
+};
+use std::hint::black_box;
+
+// ------ CONTENT
+
+// --- common
+
+fn get_map(n_square: usize) -> CMap2<FloatType> {
+    GridBuilder::unit_squares(n_square).build2().unwrap()
+}
+
+// --- constructor group
+
+#[library_benchmark]
+#[benches::with_setup(args = [16, 32, 64, 128, 256, 512])]
+fn new(n_squares: usize) -> CMap2<FloatType> {
+    black_box(CMap2::new(n_squares.pow(2) * 4))
+}
+
+#[library_benchmark]
+#[benches::with_setup(args = [16, 32, 64, 128, 256, 512])]
+fn grid(n_squares: usize) -> CMap2<FloatType> {
+    black_box(GridBuilder::unit_squares(n_squares).build2().unwrap())
+}
+
+#[library_benchmark]
+#[benches::with_setup(args = [16, 32, 64, 128, 256, 512])]
+fn tet_grid(n_squares: usize) -> CMap2<FloatType> {
+    black_box(GridBuilder::split_unit_squares(n_squares).build2().unwrap())
+}
+
+library_benchmark_group!(
+    name = bench_constructors;
+    benchmarks =
+        new,
+        grid,
+        tet_grid,
+);
+
+// --- cell fetch group
+
+#[library_benchmark]
+#[bench::small(&mut get_map(16))]
+#[bench::medium(&mut get_map(64))]
+#[bench::large(&mut get_map(256))]
+fn fetch_vertices(map: &mut CMap2<FloatType>) {
+    black_box(map.fetch_vertices());
+}
+
+#[library_benchmark]
+#[bench::small(&mut get_map(16))]
+#[bench::medium(&mut get_map(64))]
+#[bench::large(&mut get_map(256))]
+fn fetch_edges(map: &mut CMap2<FloatType>) {
+    black_box(map.fetch_edges());
+}
+
+#[library_benchmark]
+#[bench::small(&mut get_map(16))]
+#[bench::medium(&mut get_map(64))]
+#[bench::large(&mut get_map(256))]
+fn fetch_faces(map: &mut CMap2<FloatType>) {
+    black_box(map.fetch_faces());
+}
+
+library_benchmark_group!(
+    name = bench_fetches;
+    benchmarks =
+        fetch_vertices,
+        fetch_edges,
+        fetch_faces,
+);
+
+// --- i-cell group
+
+#[library_benchmark]
+#[bench::small(&get_map(16))]
+#[bench::medium(&get_map(64))]
+#[bench::large(&get_map(256))]
+fn zero_cell(map: &CMap2<FloatType>) {
+    black_box(map.i_cell::<0>(5));
+}
+
+#[library_benchmark]
+#[bench::small(&get_map(16))]
+#[bench::medium(&get_map(64))]
+#[bench::large(&get_map(256))]
+fn one_cell(map: &CMap2<FloatType>) {
+    black_box(map.i_cell::<1>(5));
+}
+
+#[library_benchmark]
+#[bench::small(&get_map(16))]
+#[bench::medium(&get_map(64))]
+#[bench::large(&get_map(256))]
+fn two_cell(map: &CMap2<FloatType>) {
+    black_box(map.i_cell::<2>(5));
+}
+
+library_benchmark_group!(
+    name = bench_icells;
+    benchmarks =
+        zero_cell,
+        one_cell,
+        two_cell,
+);
+
+// --- main
+
+main!(
+    config = LibraryBenchmarkConfig::default().flamegraph(FlamegraphConfig::default());
+    library_benchmark_groups =
+        bench_constructors,
+        bench_fetches,
+        bench_icells,
+);
