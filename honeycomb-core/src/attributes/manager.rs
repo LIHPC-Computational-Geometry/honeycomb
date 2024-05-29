e9fbf9372de6d6569e9390bca323c5e9770489ee
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-05-29 16:16:12 +0200

    add manager method skeletons

--- honeycomb-core/src/attributes/manager.rs
+++ honeycomb-core/src/attributes/manager.rs
@@ -6,14 +6,52 @@
 
 // ------ IMPORTS
 
+use crate::AttributeBind;
 use std::any::{Any, TypeId};
 use std::collections::HashMap;
 
 // ------ CONTENT
 
+#[derive(Default)]
 pub struct AttrStorageManager {
     vertices: HashMap<TypeId, Box<dyn Any>>,
     edges: HashMap<TypeId, Box<dyn Any>>,
     faces: HashMap<TypeId, Box<dyn Any>>,
     others: HashMap<TypeId, Box<dyn Any>>, // Orbit::Custom
+}
+
+impl AttrStorageManager {
+    pub fn add_storage<A: AttributeBind>(&mut self) {
+        todo!()
+    }
+
+    pub fn extend_storages(&mut self) {
+        for storage in self.vertices.values_mut() {
+            todo!()
+        }
+    }
+
+    pub fn get_storage<A: AttributeBind>(&self) {
+        todo!()
+    }
+
+    pub fn set_attribute<A: AttributeBind>(&self, id: A::IdentifierType, val: A) {
+        todo!()
+    }
+
+    pub fn insert_attribute<A: AttributeBind>(&self, id: A::IdentifierType, val: A) {
+        todo!()
+    }
+
+    pub fn get_attribute<A: AttributeBind>(&self, id: A::IdentifierType) -> A {
+        todo!()
+    }
+
+    pub fn replace_attribute<A: AttributeBind>(&self, id: A::IdentifierType, val: A) -> Option<A> {
+        todo!()
+    }
+
+    pub fn remove_attribute<A: AttributeBind>(&self, id: A::IdentifierType) -> Option<A> {
+        todo!()
+    }
 }
