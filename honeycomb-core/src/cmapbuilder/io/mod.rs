9f7d7f4eb38f877c71ad7f04ded258435f9fae3d
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-05-31 10:27:26 +0200

    update CMap2 implementations
    `new` is now `pub(crate)`
    `from_vtk_file` is now deleted

--- honeycomb-core/src/cmapbuilder/io/mod.rs
+++ honeycomb-core/src/cmapbuilder/io/mod.rs
@@ -14,26 +14,6 @@ use vtkio::{IOBuffer, Vtk};
 
 // ------ CONTENT
 
-// will be deleted soon
-impl<T: CoordsFloat + 'static> CMap2<T> {
-    /// Build a [`CMap2`] from a `vtk` file.
-    ///
-    /// # Panics
-    ///
-    /// This function may panic if:
-    /// - the file cannot be loaded
-    /// - the internal building routine fails, i.e.
-    ///     - the file format is XML
-    ///     - the mesh contains one type of cell that is not supported (either because of
-    ///     dimension or orientation incompatibilities)
-    ///     - the file has major inconsistencies / errors
-    #[deprecated(note = "please use `CMapBuilder::from_vtk_file` instead")]
-    #[must_use = "constructed object is not used, consider removing this function call"]
-    pub fn from_vtk_file(file_path: impl AsRef<std::path::Path> + std::fmt::Debug) -> Self {
-        CMapBuilder::from_vtk_file(file_path).build().unwrap()
-    }
-}
-
 impl<T: CoordsFloat> CMapBuilder<T> {
     /// Import and set the VTK file that will be used when building the map.
     ///
