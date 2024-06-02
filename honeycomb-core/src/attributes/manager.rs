a7eae5a0477d0d58e61de73009b821e4b44022b9
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-06-02 21:15:58 +0200

    add impl explanation for AttrStorageManager

--- honeycomb-core/src/attributes/manager.rs
+++ honeycomb-core/src/attributes/manager.rs
@@ -16,11 +16,66 @@ pub enum ManagerError {
     DuplicateStorage(&'static str),
 }
 
+/// Main attribute storage structure.
+///
+/// This structure is used to store all generic attributes that the user may add to the
+/// combinatorial map he's building.
+///
+/// # Implementation
+///
+/// The structure uses hashmaps in order to store each attribute's dedicated storage. Which storage
+/// is used is determined by the associated type [`AttributeBind::StorageType`].
+///
+/// The key type used by the map is each attribute's [`TypeId`]. This implies that all attributes
+/// must have a different (unique) type, i.e. two decimal-valued attribute will need to be wrapped
+/// in respective dedicated structures.
+///
+/// Using the [`TypeId`] as the key value for collections yields a cleaner API, where the only
+/// argument passed to access methods is the ID of the cell of which they want the attribute. The
+/// actual attribute type is specified by passing a generic to the method. This bypasses any issues
+/// linked to literal-typed keys, such as typos, naming conventions, portability, etc.
+///
+/// Generics passed in access methods also have a secondary usage. To store heterogeneous
+/// collections, the internal hashmaps uses `Box<dyn Any>` as their value type. This requires us
+/// to cast back the stored object (implementing `Any`) to the correct collection type. This is
+/// achieved by using the associated storage type [`AttributeBind::StorageType`]. The code would
+/// look like this:
+///
+/// ```
+/// # use std::any::{Any, TypeId};
+/// # use std::collections::HashMap;
+/// # use honeycomb_core::{AttributeBind, AttributeStorage};
+/// pub struct Manager {
+///     inner: HashMap<TypeId, Box<dyn Any>>,
+/// }
+///
+/// impl Manager {
+///     pub fn add_storage<A: AttributeBind + 'static>(
+///         &mut self,
+///         size: usize,
+///     ) {
+///         let typeid = TypeId::of::<A>();
+///         let new_storage = <A as AttributeBind>::StorageType::new(size);
+///         self.inner.insert(typeid, Box::new(new_storage));
+///     }
+///
+///     pub fn get_storage<A: AttributeBind>(&self) -> &<A as AttributeBind>::StorageType {
+///         let probably_storage = &self.inner[TypeId::of::<A>()];
+///         probably_storage
+///             .downcast_ref::<<A as AttributeBind>::StorageType>()
+///             .expect("E: could not downcast generic storage to specified attribute type")
+///     }
+/// }
+/// ```
 #[derive(Default)]
 pub struct AttrStorageManager {
+    /// Vertex attributes' storages.
     vertices: HashMap<TypeId, Box<dyn Any>>,
+    /// Edges attributes' storages.
     edges: HashMap<TypeId, Box<dyn Any>>,
+    /// Faces attributes' storages.
     faces: HashMap<TypeId, Box<dyn Any>>,
+    /// Other storages.
     others: HashMap<TypeId, Box<dyn Any>>, // Orbit::Custom
 }
 
