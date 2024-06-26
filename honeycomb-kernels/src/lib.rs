b571b1c01f1dcc922a4181cc9dd1d0708d93eb9e
Isaïe <isaie.muron@cea.fr>
2024-06-26 12:23:16 +0200

    feat(kernels): add new workspace member for algorithms implementation (#109)

---
+++ honeycomb-kernels/src/lib.rs
@@ -0,0 +1,11 @@
+//! # honeycomb-kernels
+//!
+//! This crate implements usual meshing algorithms using combinatorial maps as the underlying mesh
+//! representation structure. The implementation of those are provided in the core crate of the
+//! `honeycomb` [project][UG].
+//!
+//! [UG]:https://lihpc-computational-geometry.github.io/honeycomb/
+//!
+//! ## Kernels
+//!
+//! - Grid Submersion Algorithm
