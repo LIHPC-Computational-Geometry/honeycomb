d1aa9334a7396aa1282d059998ee36256898a5de
Isaïe <isaie.muron@cea.fr>
2024-07-24 20:32:37 +0200

    feat: add example for the `grisubal` kernel (#120)
    * add grisubal example file
    
    * add vtk input of the grisubal example

---
+++ examples/examples/kernels/grisubal.rs
@@ -0,0 +1,16 @@
+use honeycomb_kernels::grisubal;
+use honeycomb_render::{RenderParameters, SmaaMode};
+
+fn main() {
+    let map = grisubal::<f64>("assets/rectangle.vtk", false, (1., 1.), None);
+
+    let render_params = RenderParameters {
+        smaa_mode: SmaaMode::Smaa1X,
+        relative_resize: false,
+        shrink_factor: 0.05,
+        arrow_headsize: 0.01,
+        arrow_thickness: 0.005,
+    };
+
+    honeycomb_render::launch(render_params, Some(&map));
+}
