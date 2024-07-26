3b470542dadfde3cb156daca11a2c9dfae8b8a3c
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-07-26 16:15:29 +0200

    fix(examples): adjust `grisubal` call arguments

--- examples/examples/kernels/grisubal.rs
+++ examples/examples/kernels/grisubal.rs
@@ -2,7 +2,7 @@ use honeycomb_kernels::grisubal;
 use honeycomb_render::{RenderParameters, SmaaMode};
 
 fn main() {
-    let map = grisubal::<f64>("assets/rectangle.vtk", false, (1., 1.), None);
+    let map = grisubal::<f64>("assets/rectangle.vtk", (1., 1.), None);
 
     let render_params = RenderParameters {
         smaa_mode: SmaaMode::Smaa1X,
