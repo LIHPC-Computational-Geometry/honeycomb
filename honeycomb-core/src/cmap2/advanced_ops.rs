b7f498f398d9fa26463d0f9e396483a3f8422cc0
Isaïe <isaie.muron@cea.fr>
2024-07-27 09:36:22 +0200

    feat(core): add `splitn_edge` method to `CMap2` (#127)
    * implement new method for edge splitting
    
    * add doc & T value checks
    
    * add tests
    
    * adjust API to make it more flexible
    
    * add example
    
    * fix doctest

--- honeycomb-core/src/cmap2/advanced_ops.rs
+++ honeycomb-core/src/cmap2/advanced_ops.rs
@@ -10,7 +10,7 @@ use crate::{CMap2, CoordsFloat, DartIdentifier, EdgeIdentifier, Vertex2, NULL_DA
 // ------ CONTENT
 
 impl<T: CoordsFloat> CMap2<T> {
-    /// Split an edge into to segments.
+    /// Split an edge into two segments.
     ///
     /// <div class="warning">
     /// This implementation is 2D specific.
@@ -110,5 +110,133 @@ impl<T: CoordsFloat> CMap2<T> {
                 midpoint_vertex.map_or(Vertex2::average(&v1, &v2), |t| v1 + seg * t),
             );
         }
+    }
+
+    /// Split an edge into `n` segments.
+    ///
+    /// <div class="warning">
+    /// This implementation is 2D specific.
+    /// </div>
+    ///
+    /// # Arguments
+    ///
+    /// - `edge_id: EdgeIdentifier` -- Edge to split in two.
+    /// - `midpoint_vertices: I` -- Relative positions of new vertices, starting from the
+    ///   vertex of the dart sharing `edge_id` as its identifier.
+    ///
+    /// ## Generics
+    ///
+    /// - `I: Iterator<Item = T>` -- Iterator over `T` values. These should be in the `]0; 1[` open range.
+    ///
+    /// # Panics
+    ///
+    /// This method may panic if the edge upon which the operation is performed does not have two defined vertices.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use honeycomb_core::{CMap2, CMapBuilder, NULL_DART_ID, Vertex2};
+    /// // before
+    /// //    <--2---
+    /// //  1         2
+    /// //    ---1-->
+    /// let mut map: CMap2<f64> = CMapBuilder::default()
+    ///                             .n_darts(2)
+    ///                             .build()
+    ///                             .unwrap();
+    /// map.two_link(1, 2);
+    /// map.insert_vertex(1, (0.0, 0.0));
+    /// map.insert_vertex(2, (1.0, 0.0));
+    /// // split
+    /// let new_darts = map.splitn_edge(1, [0.25, 0.50, 0.75]);
+    /// // after
+    /// //    <-<-<-<
+    /// //  1 -3-4-5- 2
+    /// //    >->->->
+    /// assert_eq!(&new_darts, &[3, 4, 5]);
+    /// assert_eq!(map.vertex(3), Ok(Vertex2(0.25, 0.0)));
+    /// assert_eq!(map.vertex(4), Ok(Vertex2(0.50, 0.0)));
+    /// assert_eq!(map.vertex(5), Ok(Vertex2(0.75, 0.0)));
+    ///
+    /// assert_eq!(map.beta::<1>(1), 3);
+    /// assert_eq!(map.beta::<1>(3), 4);
+    /// assert_eq!(map.beta::<1>(4), 5);
+    /// assert_eq!(map.beta::<1>(5), NULL_DART_ID);
+    ///
+    /// assert_eq!(map.beta::<1>(2), 6);
+    /// assert_eq!(map.beta::<1>(6), 7);
+    /// assert_eq!(map.beta::<1>(7), 8);
+    /// assert_eq!(map.beta::<1>(8), NULL_DART_ID);
+    ///
+    /// assert_eq!(map.beta::<2>(1), 8);
+    /// assert_eq!(map.beta::<2>(3), 7);
+    /// assert_eq!(map.beta::<2>(4), 6);
+    /// assert_eq!(map.beta::<2>(5), 2);
+    /// ```
+    pub fn splitn_edge(
+        &mut self,
+        edge_id: EdgeIdentifier,
+        midpoint_vertices: impl IntoIterator<Item = T>,
+    ) -> Vec<DartIdentifier> {
+        // base darts making up the edge
+        let base_dart1 = edge_id as DartIdentifier;
+        let base_dart2 = self.beta::<2>(base_dart1);
+        let b1d1_old = self.beta::<1>(base_dart1);
+
+        // (*): unwrapping is ok since splitting an edge that does not have both its vertices
+        //      defined is undefined behavior, therefore panic
+        let v1 = self // (*)
+            .vertex(self.vertex_id(base_dart1))
+            .expect("E: attempt to split an edge that is not fully defined in the first place");
+        let v2 = self // (*)
+            .vertex(self.vertex_id(if base_dart2 == NULL_DART_ID {
+                b1d1_old
+            } else {
+                base_dart2
+            }))
+            .expect("E: attempt to split an edge that is not fully defined in the first place");
+        let seg = v2 - v1;
+
+        // unsew current dart
+        self.one_unlink(base_dart1);
+        if base_dart2 != NULL_DART_ID {
+            self.two_unlink(base_dart1);
+        }
+        // insert new vertices / darts on base_dart1's side
+        let mut prev_d = base_dart1;
+        let darts: Vec<DartIdentifier> = midpoint_vertices
+            .into_iter()
+            .map(|t| {
+                if (t >= T::one()) | (t <= T::zero()) {
+                    println!(
+                        "W: vertex placement for split is not in ]0;1[ -- result may be incoherent"
+                    );
+                }
+                let new_v = v1 + seg * t;
+                let new_d = self.add_free_dart();
+                self.one_link(prev_d, new_d);
+                self.insert_vertex(new_d, new_v);
+                prev_d = new_d;
+                new_d
+            })
+            .collect();
+        self.one_link(prev_d, b1d1_old);
+
+        // if b2(base_dart1) is defined, insert vertices / darts on its side too
+        if base_dart2 != NULL_DART_ID {
+            let b1d2_old = self.beta::<1>(base_dart2);
+            self.one_unlink(base_dart2);
+            let mut prev_d = base_dart2;
+            darts.iter().rev().for_each(|d| {
+                self.two_link(prev_d, *d);
+                let new_d = self.add_free_dart();
+                self.one_link(prev_d, new_d);
+                prev_d = new_d;
+            });
+            self.one_link(prev_d, b1d2_old);
+            self.two_link(prev_d, base_dart1);
+        }
+
+        darts
     }
 }
