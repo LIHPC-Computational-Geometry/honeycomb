33ccd876146e4262c4ac56bab15328e91c7f38d4
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-08-25 20:57:28 +0200

    better dart inspected data

--- honeycomb-render/src/inspector/tab.rs
+++ honeycomb-render/src/inspector/tab.rs
@@ -1,4 +1,4 @@
-use crate::capture::ecs_data::{DartBody, DartId, EdgeId, VertexId};
+use crate::capture::ecs_data::{DartBody, DartId, EdgeId, FaceId, VertexId};
 use crate::{Beta, Edge, Face, Vertex, Volume};
 use bevy::prelude::*;
 use bevy::utils::HashSet;
@@ -11,6 +11,10 @@ pub fn draw_inspected_data(
 ) {
     ui.label(egui::RichText::new("Selected Components").size(15.));
 
+    if selected_entities.is_empty() {
+        ui.separator();
+    }
+
     for entity in selected_entities {
         ui.separator();
         if world.get::<DartBody>(*entity).is_some() {
@@ -18,6 +22,26 @@ pub fn draw_inspected_data(
                 unreachable!()
             };
             ui.label(format!("Dart #{}", id.0));
+            let Some(vid) = world.get::<VertexId>(*entity) else {
+                unreachable!();
+            };
+            let Some(eid) = world.get::<EdgeId>(*entity) else {
+                unreachable!();
+            };
+            let Some(fid) = world.get::<FaceId>(*entity) else {
+                unreachable!();
+            };
+            egui::Grid::new("dart_grid").num_columns(4).show(ui, |ui| {
+                ui.label("i-cells");
+                ui.label("Vertex");
+                ui.label("Edge");
+                ui.label("Face");
+                ui.end_row();
+                ui.label("IDs");
+                ui.label(format!("{}", vid.0));
+                ui.label(format!("{}", eid.0));
+                ui.label(format!("{}", fid.0));
+            });
         } else if world.get::<Beta>(*entity).is_some() {
             ui.label("Beta");
         } else if world.get::<Vertex>(*entity).is_some() {
