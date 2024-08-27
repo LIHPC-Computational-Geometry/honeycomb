d67d64b972f06c1ae95645d7f24e84cca3e52737
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-08-27 08:40:37 +0200

    minimize bevy features

--- examples/examples/render/default_no_aa.rs
+++ examples/examples/render/default_no_aa.rs
@@ -2,5 +2,7 @@ use honeycomb_core::{CMap2, CMapBuilder};
 
 fn main() {
     let cmap: CMap2<f32> = CMapBuilder::unit_grid(4).build().unwrap();
-    honeycomb_render::App::default().add_capture(&cmap).run();
+    let mut app = honeycomb_render::App::default();
+    app.add_capture(&cmap);
+    app.run()
 }
