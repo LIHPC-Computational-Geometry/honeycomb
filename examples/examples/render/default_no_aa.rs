5e757a0a5c544573e09aa30f53a31bf2c2605e85
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-08-30 10:43:38 +0200

    remove all render example in favor of a single one

--- examples/examples/render/default_no_aa.rs
+++
@@ -1,8 +0,0 @@
-use honeycomb_core::{CMap2, CMapBuilder};
-
-fn main() {
-    let cmap: CMap2<f32> = CMapBuilder::unit_grid(4).build().unwrap();
-    let mut app = honeycomb_render::App::default();
-    app.add_capture(&cmap);
-    app.run()
-}
--- examples/examples/render/default_smaa1x.rs
+++
@@ -1,9 +0,0 @@
-use honeycomb_render::*;
-
-fn main() {
-    let render_params = RenderParameters {
-        smaa_mode: SmaaMode::Smaa1X,
-        ..Default::default()
-    };
-    launch::<f32>(render_params, None);
-}
