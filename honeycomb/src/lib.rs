8ce7acc9090b6e4117389cd577b2f0241b343fad
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-09-03 15:09:57 +0200

    explicitly re-export core items instead of using its prelude

--- honeycomb/src/lib.rs
+++ honeycomb/src/lib.rs
@@ -9,10 +9,22 @@ pub use honeycomb_kernels as kernels;
 pub use honeycomb_render as render;
 
 pub mod prelude {
-    pub use honeycomb_core::prelude::*;
+    // ------ CORE RE-EXPORTS
+
+    pub use honeycomb_core::attributes::{AttributeBind, AttributeUpdate};
+    pub use honeycomb_core::cmap::{
+        BuilderError, CMap2, CMapBuilder, CMapError, DartIdentifier, EdgeIdentifier,
+        FaceIdentifier, GridDescriptor, Orbit2, OrbitPolicy, VertexIdentifier, VolumeIdentifier,
+        NULL_DART_ID, NULL_EDGE_ID, NULL_FACE_ID, NULL_VERTEX_ID, NULL_VOLUME_ID,
+    };
+    pub use honeycomb_core::geometry::{CoordsError, CoordsFloat, Vector2, Vertex2};
+
+    // ------ KERNELS RE-EXPORTS
 
     #[cfg(feature = "kernels")]
     pub use honeycomb_kernels::grisubal;
+
+    // ------ RENDER RE-EXPORTS
 
     #[cfg(feature = "render")]
     pub use honeycomb_render::App;
