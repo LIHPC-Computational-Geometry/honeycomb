1aeebc79a9d048e1db8866b61d422f0e0398dbb7
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-09-30 11:14:31 +0200

    add a req check function

--- honeycomb-kernels/src/triangulation/mod.rs
+++ honeycomb-kernels/src/triangulation/mod.rs
@@ -21,6 +21,53 @@ mod fan;
 pub use ear_clipping::process_cell as earclip_cell;
 pub use fan::process_cell as fan_cell;
 pub use fan::process_convex_cell as fan_convex_cell;
+use honeycomb_core::cmap::FaceIdentifier;
+// ------ CONTENT
+
+pub enum TriangulateError {
+    AlreadyTriangulated,
+    NoEar,
+    NonFannable,
+    NotEnoughDarts(usize),
+    TooManyDarts(usize),
+    UndefinedFace(String),
+}
+
+#[allow(
+    clippy::cast_possible_wrap,
+    clippy::cast_sign_loss,
+    clippy::cast_abs_to_unsigned
+)]
+pub fn check_requirements(
+    n_darts_face: usize,
+    n_darts_allocated: usize,
+    face_id: FaceIdentifier,
+) -> Result<(), TriangulateError> {
+    match n_darts_face {
+        1 | 2 => {
+            return Err(TriangulateError::UndefinedFace(format!(
+                "face {face_id} has less than three vertices"
+            )));
+        }
+        3 => {
+            return Err(TriangulateError::AlreadyTriangulated);
+        }
+        _ => {}
+    }
+
+    // check the value of n_allocated - n_expected
+    match n_darts_allocated as isize - (n_darts_face as isize - 3) * 2 {
+        diff @ ..0 => {
+            return Err(TriangulateError::NotEnoughDarts(diff.abs() as usize));
+        }
+        0 => {}
+        diff @ 1.. => {
+            return Err(TriangulateError::TooManyDarts(diff as usize));
+        }
+    }
+
+    Ok(())
+}
 
 // ------ TESTS
 
