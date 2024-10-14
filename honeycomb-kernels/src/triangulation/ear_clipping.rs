1eb96db806cc5202bbe0d512cd67691d854a7695
Isaïe <isaie.muron@cea.fr>
2024-10-14 10:07:11 +0200

    refactor!: cleanup error enums (#190)
    * add `thiserror` dep
    
    * derive Error for TriangulateError
    
    * remove FaceId from error message
    
    it's passed as argument of the triangulate function anyway
    
    * derive Error for GrisubalError
    
    * fix kernel warnings
    
    * derive Error for CoordsError
    
    * derive Error for BuilderError
    
    * fix clippy lints
    
    * fix tests

--- honeycomb-kernels/src/triangulation/ear_clipping.rs
+++ honeycomb-kernels/src/triangulation/ear_clipping.rs
@@ -64,10 +64,10 @@ pub fn process_cell<T: CoordsFloat>(
     let mut n = darts.len();
 
     // early checks - check # of darts & face size
-    check_requirements(n, new_darts.len(), face_id)?;
+    check_requirements(n, new_darts.len())?;
 
     // get associated vertices - check for undefined vertices
-    let mut vertices = fetch_face_vertices(cmap, &darts, face_id)?;
+    let mut vertices = fetch_face_vertices(cmap, &darts)?;
 
     let mut ndart_id = new_darts[0];
     while n > 3 {
