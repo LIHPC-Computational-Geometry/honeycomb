2becad74b63b0793614b4122dcd61b0196bdc338
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-10-14 14:32:03 +0200

    add binary to benchmark the grid builder

---
+++ benches/src/builder.rs
@@ -0,0 +1,26 @@
+use honeycomb::prelude::{CMapBuilder, GridDescriptor};
+
+fn main() {
+    // ./binary nx ny split
+    let grid = {
+        let args: Vec<String> = std::env::args().collect();
+        match (args.get(1), args.get(2), args.get(3)) {
+            (Some(nx), Some(ny), split) => {
+                let nx = nx.parse().unwrap();
+                let ny = ny.parse().unwrap();
+                GridDescriptor::default()
+                    .n_cells([nx, ny, 0])
+                    .lens([1.0, 1.0, 1.0])
+                    .split_quads(split.is_some())
+            }
+            _ => panic!("E: specifify at least the number of cells along X and Y axes"),
+        }
+    };
+
+    let map = CMapBuilder::default()
+        .grid_descriptor(grid)
+        .build()
+        .unwrap();
+
+    std::hint::black_box(map);
+}
