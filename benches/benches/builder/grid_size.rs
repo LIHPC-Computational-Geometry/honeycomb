4b31a11693cc5c7096a41a1b1c35889b82018119
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-10-15 08:45:30 +0200

    add builder benchmarks

---
+++ benches/benches/builder/grid_size.rs
@@ -0,0 +1,34 @@
+// ------ IMPORTS
+
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use honeycomb::prelude::CMap2;
+use honeycomb_benches::FloatType;
+use honeycomb_core::cmap::CMapBuilder;
+// ------ CONTENT
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("builder-grid-size");
+
+    for multiplier in 7..13 {
+        let size = 2_u64.pow(multiplier);
+        group.throughput(Throughput::Elements(size.pow(2))); // throughoutput = number of cells
+        group.bench_with_input(BenchmarkId::new("unit-squares", ""), &size, |b, size| {
+            b.iter(|| {
+                let mut map: CMap2<FloatType> =
+                    CMapBuilder::unit_grid(*size as usize).build().unwrap();
+                black_box(&mut map);
+            })
+        });
+        group.bench_with_input(BenchmarkId::new("unit-triangles", ""), &size, |b, size| {
+            b.iter(|| {
+                let mut map: CMap2<FloatType> =
+                    CMapBuilder::unit_triangles(*size as usize).build().unwrap();
+                black_box(&mut map);
+            })
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
