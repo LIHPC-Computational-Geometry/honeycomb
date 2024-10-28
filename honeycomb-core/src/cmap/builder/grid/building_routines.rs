c69c6d01def65c53c19aa0ccbc90c03ee7e0cef5
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-10-28 15:40:16 +0100

    split last row/col cases from the grid building loop
    extract some function common to all cases

--- honeycomb-core/src/cmap/builder/grid/building_routines.rs
+++ honeycomb-core/src/cmap/builder/grid/building_routines.rs
@@ -8,6 +8,7 @@ use crate::{attributes::AttrStorageManager, geometry::CoordsFloat};
 // ------ CONTENT
 
 /// Internal grid-building routine
+#[allow(clippy::too_many_lines)]
 pub fn build_2d_grid<T: CoordsFloat>(
     origin: Vertex2<T>,
     [n_square_x, n_square_y]: [usize; 2],
@@ -17,46 +18,20 @@ pub fn build_2d_grid<T: CoordsFloat>(
     let mut map: CMap2<T> =
         CMap2::new_with_undefined_attributes(4 * n_square_x * n_square_y, manager);
 
-    (0..n_square_y)
+    // regular iterations (skip last row/col)
+    (0..n_square_y - 1)
         // flatten the loop to expose more parallelism
-        // this is not a quantified/benchmarked upgrade, just a seemingly good change
-        .flat_map(|y_idx| (0..n_square_x).map(move |x_idx| (y_idx, x_idx)))
+        .flat_map(|y_idx| (0..n_square_x - 1).map(move |x_idx| (y_idx, x_idx)))
         .for_each(|(y_idx, x_idx)| {
-            // compute dart IDs of the cell
-            let d1 = (1 + 4 * x_idx + n_square_x * 4 * y_idx) as DartIdentifier;
-            let (d2, d3, d4) = (d1 + 1, d1 + 2, d1 + 3);
-            // are we on the last col / row / both?
-            let last_column = x_idx == n_square_x - 1;
-            let last_row = y_idx == n_square_y - 1;
+            // build basic topology & fetch dart IDs of the cell
+            let [d1, d2, d3, _] = build_square_core(&mut map, n_square_x, [x_idx, y_idx]);
 
-            // edit topology
-            // d1
-            map.set_beta::<0>(d1, d4);
-            map.set_beta::<1>(d1, d2);
-            // d2
-            map.set_beta::<0>(d2, d1);
-            map.set_beta::<1>(d2, d3);
-            // d3
-            map.set_beta::<0>(d3, d2);
-            map.set_beta::<1>(d3, d4);
-            // d4
-            map.set_beta::<0>(d4, d3);
-            map.set_beta::<1>(d4, d1);
-            // if there is a right neighbor, sew sew
-            if !last_column {
-                let right_neighbor = d2 + 6;
-                map.set_beta::<2>(d2, right_neighbor);
-                map.set_beta::<2>(right_neighbor, d2);
-            }
-            // if there is an up neighbor, sew sew
-            if !last_row {
-                let up_neighbor = d1 + (4 * n_square_x) as DartIdentifier;
-                map.set_beta::<2>(d3, up_neighbor);
-                map.set_beta::<2>(up_neighbor, d3);
-            }
+            // sew to right & up neighbors
+            build_square_sew_right(&mut map, d2);
+            build_square_sew_up(&mut map, d3, n_square_x);
 
             // edit geometry
-            let vertex_id = map.vertex_id(d1);
+            let vertex_id = map.vertex_id(d1); // bottom left
             map.insert_vertex(
                 vertex_id,
                 origin
@@ -65,44 +40,115 @@ pub fn build_2d_grid<T: CoordsFloat>(
                         T::from(y_idx).unwrap() * len_per_y,
                     ),
             );
-            if last_column {
-                // that last column of 0-cell needs special treatment
-                // bc there are no "horizontal" associated dart
-                let vertex_id = map.vertex_id(d2);
-                map.insert_vertex(
-                    vertex_id,
-                    origin
-                        + Vector2(
-                            T::from(x_idx + 1).unwrap() * len_per_x,
-                            T::from(y_idx).unwrap() * len_per_y,
-                        ),
-                );
-            }
-            if last_row {
-                // same as the case on x
-                let vertex_id = map.vertex_id(d4);
-                map.insert_vertex(
-                    vertex_id,
-                    origin
-                        + Vector2(
-                            T::from(x_idx).unwrap() * len_per_x,
-                            T::from(y_idx + 1).unwrap() * len_per_y,
-                        ),
-                );
-            }
-            if last_row & last_column {
-                // need to do the upper right corner
-                let vertex_id = map.vertex_id(d3);
-                map.insert_vertex(
-                    vertex_id,
-                    origin
-                        + Vector2(
-                            T::from(x_idx + 1).unwrap() * len_per_x,
-                            T::from(y_idx + 1).unwrap() * len_per_y,
-                        ),
-                );
-            }
         });
+
+    // last row (except top right square)
+    (0..n_square_x - 1).for_each(|x_idx| {
+        let y_idx = n_square_y - 1;
+
+        // build basic topology & fetch dart IDs of the cell
+        let [d1, d2, _, d4] = build_square_core(&mut map, n_square_x, [x_idx, y_idx]);
+
+        // sew to right neighbor only
+        build_square_sew_right(&mut map, d2);
+
+        // edit geometry
+        let vertex_id = map.vertex_id(d1); // bottom left
+        map.insert_vertex(
+            vertex_id,
+            origin
+                + Vector2(
+                    T::from(x_idx).unwrap() * len_per_x,
+                    T::from(y_idx).unwrap() * len_per_y,
+                ),
+        );
+        let vertex_id = map.vertex_id(d4); // top left
+        map.insert_vertex(
+            vertex_id,
+            origin
+                + Vector2(
+                    T::from(x_idx).unwrap() * len_per_x,
+                    T::from(y_idx + 1).unwrap() * len_per_y,
+                ),
+        );
+    });
+
+    // last col (except top right square)
+    (0..n_square_y - 1).for_each(|y_idx| {
+        let x_idx = n_square_x - 1;
+
+        // build basic topology & fetch dart IDs of the cell
+        let [d1, d2, d3, _] = build_square_core(&mut map, n_square_x, [x_idx, y_idx]);
+
+        // sew to up neighbor only
+        build_square_sew_up(&mut map, d3, n_square_x);
+
+        // edit geometry
+        let vertex_id = map.vertex_id(d1); // bottom left
+        map.insert_vertex(
+            vertex_id,
+            origin
+                + Vector2(
+                    T::from(x_idx).unwrap() * len_per_x,
+                    T::from(y_idx).unwrap() * len_per_y,
+                ),
+        );
+        let vertex_id = map.vertex_id(d2); // bottom right
+        map.insert_vertex(
+            vertex_id,
+            origin
+                + Vector2(
+                    T::from(x_idx + 1).unwrap() * len_per_x,
+                    T::from(y_idx).unwrap() * len_per_y,
+                ),
+        );
+    });
+
+    // most top right cell
+    {
+        let (x_idx, y_idx) = (n_square_x - 1, n_square_y - 1);
+
+        // build basic topology & fetch dart IDs of the cell
+        let [d1, d2, d3, d4] = build_square_core(&mut map, n_square_x, [x_idx, y_idx]);
+
+        // edit geometry
+        let vertex_id = map.vertex_id(d1); // bottom left
+        map.insert_vertex(
+            vertex_id,
+            origin
+                + Vector2(
+                    T::from(x_idx).unwrap() * len_per_x,
+                    T::from(y_idx).unwrap() * len_per_y,
+                ),
+        );
+        let vertex_id = map.vertex_id(d2); // bottom right
+        map.insert_vertex(
+            vertex_id,
+            origin
+                + Vector2(
+                    T::from(x_idx + 1).unwrap() * len_per_x,
+                    T::from(y_idx).unwrap() * len_per_y,
+                ),
+        );
+        let vertex_id = map.vertex_id(d4); // top left
+        map.insert_vertex(
+            vertex_id,
+            origin
+                + Vector2(
+                    T::from(x_idx).unwrap() * len_per_x,
+                    T::from(y_idx + 1).unwrap() * len_per_y,
+                ),
+        );
+        let vertex_id = map.vertex_id(d3); // top right
+        map.insert_vertex(
+            vertex_id,
+            origin
+                + Vector2(
+                    T::from(x_idx + 1).unwrap() * len_per_x,
+                    T::from(y_idx + 1).unwrap() * len_per_y,
+                ),
+        );
+    }
 
     // check the number of built faces
     // this is set as debug only because the operation cost scales with map size
@@ -110,6 +156,53 @@ pub fn build_2d_grid<T: CoordsFloat>(
     debug_assert_eq!(map.fetch_faces().identifiers.len(), n_square_x * n_square_y);
 
     map
+}
+
+#[allow(clippy::inline_always)]
+#[inline(always)] // seems like this is required to match the actual inline perf
+fn build_square_core<T: CoordsFloat>(
+    map: &mut CMap2<T>,
+    n_square_x: usize,
+    [x_idx, y_idx]: [usize; 2],
+) -> [DartIdentifier; 4] {
+    let d1 = (1 + 4 * x_idx + n_square_x * 4 * y_idx) as DartIdentifier;
+    let (d2, d3, d4) = (d1 + 1, d1 + 2, d1 + 3);
+
+    // edit topology
+    // d1
+    map.set_beta::<0>(d1, d4);
+    map.set_beta::<1>(d1, d2);
+    // d2
+    map.set_beta::<0>(d2, d1);
+    map.set_beta::<1>(d2, d3);
+    // d3
+    map.set_beta::<0>(d3, d2);
+    map.set_beta::<1>(d3, d4);
+    // d4
+    map.set_beta::<0>(d4, d3);
+    map.set_beta::<1>(d4, d1);
+
+    [d1, d2, d3, d4]
+}
+
+#[allow(clippy::inline_always)]
+#[inline(always)] // seems like this is required to match the actual inline perf
+fn build_square_sew_right<T: CoordsFloat>(map: &mut CMap2<T>, dart: DartIdentifier) {
+    let right_neighbor = dart + 6;
+    map.set_beta::<2>(dart, right_neighbor);
+    map.set_beta::<2>(right_neighbor, dart);
+}
+
+#[allow(clippy::inline_always)]
+#[inline(always)] // seems like this is required to match the actual inline perf
+fn build_square_sew_up<T: CoordsFloat>(
+    map: &mut CMap2<T>,
+    dart: DartIdentifier,
+    n_square_x: usize,
+) {
+    let up_neighbor = dart - 2 + 4 * n_square_x as DartIdentifier; // d1 + 4*nx
+    map.set_beta::<2>(dart, up_neighbor);
+    map.set_beta::<2>(up_neighbor, dart);
 }
 
 /// Internal grid-building routine
