08106cfea3fc64c824b7d86029ad483a2617b4a1
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-10-31 15:40:30 +0100

    add doc build script

---
+++ docs/build.sh
@@ -0,0 +1,11 @@
+# Generate docs
+
+cargo +nightly doc --all --no-deps --all-features
+mdbook build user-guide/
+
+# Add stuff to the deployed folder
+
+mv user-guide/book target/doc/user-guide
+cp docs/index.html target/doc
+cp docs/index.css target/doc
+cp docs/theme.js target/doc
