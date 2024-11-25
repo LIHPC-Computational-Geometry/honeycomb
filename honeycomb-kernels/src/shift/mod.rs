fdd7912a5bf3e9ca0a4b8b6428efa697e615c91a
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-11-25 16:01:02 +0100

    bench shift function

--- honeycomb-kernels/src/shift/mod.rs
+++ honeycomb-kernels/src/shift/mod.rs
@@ -1,1 +1,3 @@
 mod neighbors_average;
+
+pub use neighbors_average::shift as shift_vertices_to_neigh_avg;
--- honeycomb-kernels/src/shift/neighbors_average.rs
+++ honeycomb-kernels/src/shift/neighbors_average.rs
@@ -3,13 +3,14 @@ use honeycomb_core::{
     prelude::{CoordsFloat, Vertex2},
     stm::atomically,
 };
+use rayon::prelude::*;
 
-pub fn shift<T: CoordsFloat>(cmap: &CMap2<T>) {
+pub fn shift<T: CoordsFloat>(cmap: &CMap2<T>, n_rounds: usize) {
     // fetch all vertices that are not on the boundary of the map
     let vertices: Vec<VertexIdType> = cmap
         .fetch_vertices()
         .identifiers
-        .into_iter()
+        .into_par_iter()
         .filter(|v| {
             !Orbit2::new(&cmap, OrbitPolicy::Vertex, *v as DartIdType)
                 .any(|d| cmap.beta::<2>(d) == NULL_DART_ID)
@@ -17,7 +18,7 @@ pub fn shift<T: CoordsFloat>(cmap: &CMap2<T>) {
         .collect();
 
     let neighbors: Vec<Vec<VertexIdType>> = vertices
-        .iter()
+        .par_iter()
         .map(|v| {
             Orbit2::new(&cmap, OrbitPolicy::Vertex, *v as DartIdType)
                 .map(|d| cmap.vertex_id(cmap.beta::<2>(d)))
@@ -26,20 +27,28 @@ pub fn shift<T: CoordsFloat>(cmap: &CMap2<T>) {
         .collect();
 
     // main loop
-    vertices
-        .iter()
-        .zip(neighbors.iter())
-        .for_each(|(vid, neigh)| {
-            atomically(|trans| {
-                let mut new_val: Vertex2<T> = Vertex2::default();
-                for v in neigh {
-                    let vertex = cmap.read_vertex(trans, *v)?.unwrap();
-                    new_val.0 += vertex.0;
-                    new_val.1 += vertex.1;
-                }
-                new_val.0 /= T::from(neigh.len()).unwrap();
-                new_val.1 /= T::from(neigh.len()).unwrap();
-                cmap.write_vertex(trans, *vid, new_val)
+    let mut round = 0;
+    loop {
+        vertices
+            .iter()
+            .zip(neighbors.iter())
+            .par_bridge()
+            .for_each(|(vid, neigh)| {
+                atomically(|trans| {
+                    let mut new_val: Vertex2<T> = Vertex2::default();
+                    for v in neigh {
+                        let vertex = cmap.read_vertex(trans, *v)?.unwrap();
+                        new_val.0 += vertex.0;
+                        new_val.1 += vertex.1;
+                    }
+                    new_val.0 /= T::from(neigh.len()).unwrap();
+                    new_val.1 /= T::from(neigh.len()).unwrap();
+                    cmap.write_vertex(trans, *vid, new_val)
+                });
             });
-        });
+        round += 1;
+        if round >= n_rounds {
+            break;
+        }
+    }
 }
