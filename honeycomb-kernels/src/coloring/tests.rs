c07113919d9a9149415db78d1930c8aed379c758
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-12-04 14:32:25 +0100

    add a second map to the test
    there's an issue in neighbors computation bc nodes on the edge have a
    single oriented dart as links

--- honeycomb-kernels/src/coloring/tests.rs
+++ honeycomb-kernels/src/coloring/tests.rs
@@ -5,23 +5,54 @@ use crate::coloring::{color_dsatur, Color};
 // test many different maps for basic properties of the result
 #[test]
 fn dsatur_invariants() {
-    let mut map: CMap2<f64> = CMapBuilder::unit_grid(64).build().unwrap();
-    let colors = 0..=color_dsatur(&mut map);
-    let vertices = map.fetch_vertices().identifiers.clone();
+    {
+        let mut map: CMap2<f64> = CMapBuilder::unit_grid(64).build().unwrap();
+        let colors = 0..=color_dsatur(&mut map);
+        let vertices = map.fetch_vertices().identifiers.clone();
 
-    // all vertices are colored, colors being in (0..=cmax)
-    assert!(vertices
-        .iter()
-        .map(|id| map.force_read_attribute::<Color>(*id))
-        .all(|c| c.is_some_and(|Color(val)| colors.contains(&val))));
-    // for all vertices v, v has no neighbor with the same color
-    assert!(vertices
-        .iter()
-        .map(|id| (
-            map.force_read_attribute::<Color>(*id).unwrap(),
-            Orbit2::new(&map, OrbitPolicy::Vertex, *id).map(|d| map
-                .force_read_attribute::<Color>(map.vertex_id(map.beta::<1>(d)))
-                .unwrap())
-        ))
-        .all(|(c, mut cns)| cns.all(|cn| cn != c)));
+        // all vertices are colored, colors being in (0..=cmax)
+        assert!(vertices
+            .iter()
+            .map(|id| map.force_read_attribute::<Color>(*id))
+            .all(|c| c.is_some_and(|Color(val)| colors.contains(&val))));
+        // for all vertices v, v has no neighbor with the same color
+        assert!(vertices
+            .iter()
+            .map(|id| (
+                map.force_read_attribute::<Color>(*id).unwrap(),
+                Orbit2::new(&map, OrbitPolicy::Vertex, *id).map(|d| map
+                    .force_read_attribute::<Color>(map.vertex_id(map.beta::<1>(d)))
+                    .unwrap())
+            ))
+            .all(|(c, mut cns)| cns.all(|cn| cn != c)));
+    }
+
+    {
+        let mut map: CMap2<f64> = CMapBuilder::unit_triangles(2).build().unwrap();
+        let colors = 0..=color_dsatur(&mut map);
+        let vertices = map.fetch_vertices().identifiers.clone();
+
+        for &v in &vertices {
+            println!(
+                "Vertex {v} has color {:?}",
+                map.force_read_attribute::<Color>(v).unwrap()
+            );
+        }
+
+        // all vertices are colored, colors being in (0..=cmax)
+        assert!(vertices
+            .iter()
+            .map(|id| map.force_read_attribute::<Color>(*id))
+            .all(|c| c.is_some_and(|Color(val)| colors.contains(&val))));
+        // for all vertices v, v has no neighbor with the same color
+        assert!(vertices
+            .iter()
+            .map(|id| (
+                map.force_read_attribute::<Color>(*id).unwrap(),
+                Orbit2::new(&map, OrbitPolicy::Vertex, *id).map(|d| map
+                    .force_read_attribute::<Color>(map.vertex_id(map.beta::<1>(d)))
+                    .unwrap())
+            ))
+            .all(|(c, mut cns)| cns.all(|cn| cn != c)));
+    }
 }
