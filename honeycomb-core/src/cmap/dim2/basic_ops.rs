78a494e5d1b6fead01fc5131fbe551b0d5d0fb63
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-12-11 11:02:21 +0100

    update cell ID computation

--- honeycomb-core/src/cmap/dim2/basic_ops.rs
+++ honeycomb-core/src/cmap/dim2/basic_ops.rs
@@ -281,110 +281,84 @@ impl<T: CoordsFloat> CMap2<T> {
 
 /// **I-cell-related methods**
 impl<T: CoordsFloat> CMap2<T> {
-    #[allow(clippy::missing_panics_doc)]
-    /// Fetch vertex identifier associated to a given dart.
+    /// Compute the associated vertex ID of a given dart.
     ///
-    /// # Arguments
-    ///
-    /// - `dart_id: DartIdentifier` -- Identifier of a given dart.
-    ///
-    /// # Return
-    ///
-    /// Return the identifier of the associated vertex.
-    ///
-    /// ## Note on cell identifiers
-    ///
-    /// Cells identifiers are defined as the smallest identifier among the darts that make up the
-    /// cell. This definition has three interesting properties:
-    ///
-    /// - A given cell ID can be computed from any dart of the cell, i.e. all darts have an
-    ///   associated cell ID.
-    /// - Cell IDs are not affected by the order of traversal of the map.
-    /// - Because the ID is computed in real time, there is no need to store cell IDs and ensure
-    ///   that the storage is consistent / up to date.
-    ///
-    /// These properties come at the literal cost of the computation routine, which is:
-    /// 1. a BFS to compute a given orbit
-    /// 2. a minimum computation on the IDs composing the orbit
-    ///
+    /// This corresponds to the minimum dart ID among darts composing the topological vertex.
     #[must_use = "returned value is not used, consider removing this method call"]
     pub fn vertex_id(&self, dart_id: DartIdType) -> VertexIdType {
-        // unwraping the result is safe because the orbit is always non empty
-        Orbit2::<'_, T>::new(self, OrbitPolicy::Vertex, dart_id)
-            .min()
-            .expect("E: unreachable") as VertexIdType
+        let mut min = dart_id;
+        let mut crt = self.beta::<1>(self.beta::<2>(dart_id));
+
+        // we first iterate in direct direction (B1oB2)
+        while crt != NULL_DART_ID && crt != dart_id {
+            min = min.min(crt);
+            crt = self.beta::<1>(self.beta::<2>(crt));
+        }
+        // if we landed on the null dart, the vertex is open
+        // we need to iterate in the opposite dir (B2oB0)
+        if crt == NULL_DART_ID {
+            crt = self.beta::<2>(self.beta::<0>(dart_id));
+            while crt != NULL_DART_ID {
+                min = min.min(crt);
+                crt = self.beta::<2>(self.beta::<0>(crt));
+            }
+        }
+
+        min
     }
 
-    /// Atomically compute the associated vertex ID.
-    pub(crate) fn vertex_id_transac(
+    /// Compute the associated vertex ID of a given dart.
+    ///
+    /// This corresponds to the minimum dart ID among darts composing the topological vertex.
+    pub fn vertex_id_transac(
         &self,
         trans: &mut Transaction,
         dart_id: DartIdType,
     ) -> Result<VertexIdType, StmError> {
-        let mut marked = BTreeSet::<DartIdType>::new();
-        marked.insert(NULL_DART_ID); // we don't want to include the null dart in the orbit
-        marked.insert(dart_id); // we're starting here, so we mark it beforehand
-        let mut pending = VecDeque::from([dart_id]);
-        while let Some(d) = pending.pop_front() {
-            let image1 = self.betas[(1, self.betas[(2, d)].read(trans)?)].read(trans)?;
-            if marked.insert(image1) {
-                // if true, we did not see this dart yet
-                // i.e. we need to visit it later
-                pending.push_back(image1);
-            }
-            let image2 = self.betas[(2, self.betas[(0, d)].read(trans)?)].read(trans)?;
-            if marked.insert(image2) {
-                // if true, we did not see this dart yet
-                // i.e. we need to visit it later
-                pending.push_back(image2);
+        let mut min = dart_id;
+        let mut crt = self.betas[(1, self.betas[(2, dart_id)].read(trans)?)].read(trans)?;
+
+        // we first iterate in direct direction (B1oB2)
+        while crt != NULL_DART_ID && crt != dart_id {
+            min = min.min(crt);
+            crt = self.betas[(1, self.betas[(2, crt)].read(trans)?)].read(trans)?;
+        }
+        // if we landed on the null dart, the vertex is open
+        // we need to iterate in the opposite dir (B2oB0)
+        if crt == NULL_DART_ID {
+            crt = self.betas[(2, self.betas[(0, dart_id)].read(trans)?)].read(trans)?;
+            while crt != NULL_DART_ID {
+                min = min.min(crt);
+                crt = self.betas[(2, self.betas[(0, crt)].read(trans)?)].read(trans)?;
             }
         }
-        marked.remove(&NULL_DART_ID);
-        Ok(marked.into_iter().min().unwrap() as VertexIdType)
+
+        Ok(min)
     }
 
-    #[allow(clippy::missing_panics_doc)]
-    /// Fetch edge associated to a given dart.
+    /// Compute the associated edge ID of a given dart.
     ///
-    /// # Arguments
-    ///
-    /// - `dart_id: DartIdentifier` -- Identifier of a given dart.
-    ///
-    /// # Return
-    ///
-    /// Return the identifier of the associated edge.
-    ///
-    /// ## Note on cell identifiers
-    ///
-    /// Cells identifiers are defined as the smallest identifier among the darts that make up the
-    /// cell. This definition has three interesting properties:
-    ///
-    /// - A given cell ID can be computed from any dart of the cell, i.e. all darts have an
-    ///   associated cell ID.
-    /// - Cell IDs are not affected by the order of traversal of the map.
-    /// - Because the ID is computed in real time, there is no need to store cell IDs and ensure
-    ///   that the storage is consistent / up to date.
-    ///
-    /// These properties come at the literal cost of the computation routine, which is:
-    /// 1. a BFS to compute a given orbit
-    /// 2. a minimum computation on the IDs composing the orbit
-    ///
+    /// This corresponds to the minimum dart ID among darts composing the topological edge.
     #[must_use = "returned value is not used, consider removing this method call"]
     pub fn edge_id(&self, dart_id: DartIdType) -> EdgeIdType {
-        // unwraping the result is safe because the orbit is always non empty
-        Orbit2::<'_, T>::new(self, OrbitPolicy::Edge, dart_id)
-            .min()
-            .expect("E: unreachable") as EdgeIdType
+        let b2 = self.beta::<2>(dart_id);
+        if b2 == NULL_DART_ID {
+            dart_id as EdgeIdType
+        } else {
+            b2.min(dart_id) as EdgeIdType
+        }
     }
 
-    /// Atomically compute the associated edge ID.
-    pub(crate) fn edge_id_transac(
+    /// Compute the associated edge ID of a given dart.
+    ///
+    /// This corresponds to the minimum dart ID among darts composing the topological edge.
+    pub fn edge_id_transac(
         &self,
         trans: &mut Transaction,
         dart_id: DartIdType,
     ) -> Result<EdgeIdType, StmError> {
         // optimizing this one bc I'm tired
-        let b2 = self.betas[(2, dart_id)].read(trans)?;
+        let b2 = self.beta_transac::<2>(trans, dart_id)?;
         if b2 == NULL_DART_ID {
             Ok(dart_id as EdgeIdType)
         } else {
@@ -392,62 +366,59 @@ impl<T: CoordsFloat> CMap2<T> {
         }
     }
 
-    #[allow(clippy::missing_panics_doc)]
-    /// Fetch face associated to a given dart.
+    /// Compute the associated face ID of a given dart.
     ///
-    /// # Arguments
-    ///
-    /// - `dart_id: DartIdentifier` -- Identifier of a given dart.
-    ///
-    /// # Return
-    ///
-    /// Return the identifier of the associated face.
-    ///
-    /// ## Note on cell identifiers
-    ///
-    /// Cells identifiers are defined as the smallest identifier among the darts that make up the
-    /// cell. This definition has three interesting properties:
-    ///
-    /// - A given cell ID can be computed from any dart of the cell, i.e. all darts have an
-    ///   associated cell ID.
-    /// - Cell IDs are not affected by the order of traversal of the map.
-    /// - Because the ID is computed in real time, there is no need to store cell IDs and ensure
-    ///   that the storage is consistent / up to date.
-    ///
-    /// These properties come at the literal cost of the computation routine, which is:
-    /// 1. a BFS to compute a given orbit
-    /// 2. a minimum computation on the IDs composing the orbit
-    ///
+    /// This corresponds to the minimum dart ID among darts composing the topological face.
     #[must_use = "returned value is not used, consider removing this method call"]
     pub fn face_id(&self, dart_id: DartIdType) -> FaceIdType {
-        // unwraping the result is safe because the orbit is always non empty
-        Orbit2::<'_, T>::new(self, OrbitPolicy::Face, dart_id)
-            .min()
-            .expect("E: unreachable") as FaceIdType
+        let mut min = dart_id;
+        let mut crt = self.beta::<1>(dart_id);
+
+        // we first iterate in direct direction (B1)
+        while crt != NULL_DART_ID && crt != dart_id {
+            min = min.min(crt);
+            crt = self.beta::<1>(crt);
+        }
+        // if we landed on the null dart, the face is open
+        // we need to iterate in the opposite dir (B0)
+        if crt == NULL_DART_ID {
+            crt = self.beta::<0>(dart_id);
+            while crt != NULL_DART_ID {
+                min = min.min(crt);
+                crt = self.beta::<0>(crt);
+            }
+        }
+
+        min
     }
 
-    #[allow(unused)]
-    /// Atomically compute the associated face ID.
-    pub(crate) fn face_id_transac(
+    /// Compute the associated face ID of a given dart.
+    ///
+    /// This corresponds to the minimum dart ID among darts composing the topological face.
+    pub fn face_id_transac(
         &self,
         trans: &mut Transaction,
         dart_id: DartIdType,
     ) -> Result<FaceIdType, StmError> {
-        let mut marked = BTreeSet::<DartIdType>::new();
-        marked.insert(NULL_DART_ID); // we don't want to include the null dart in the orbit
-        marked.insert(dart_id); // we're starting here, so we mark it beforehand
-        let mut pending = VecDeque::from([dart_id]);
-        while let Some(d) = pending.pop_front() {
-            // WE ASSUME THAT THE FACE IS COMPLETE
-            let image = self.betas[(1, d)].read(trans)?;
-            if marked.insert(image) {
-                // if true, we did not see this dart yet
-                // i.e. we need to visit it later
-                pending.push_back(image);
+        let mut min = dart_id;
+        let mut crt = self.beta_transac::<1>(trans, dart_id)?;
+
+        // we first iterate in direct direction (B1)
+        while crt != NULL_DART_ID && crt != dart_id {
+            min = min.min(crt);
+            crt = self.beta_transac::<1>(trans, crt)?;
+        }
+        // if we landed on the null dart, the face is open
+        // we need to iterate in the opposite dir (B0)
+        if crt == NULL_DART_ID {
+            crt = self.beta_transac::<0>(trans, dart_id)?;
+            while crt != NULL_DART_ID {
+                min = min.min(crt);
+                crt = self.beta_transac::<0>(trans, crt)?;
             }
         }
-        marked.remove(&NULL_DART_ID);
-        Ok(marked.into_iter().min().unwrap() as FaceIdType)
+
+        Ok(min)
     }
 
     /// Return an [`Orbit2`] object that can be used to iterate over darts of an i-cell.
