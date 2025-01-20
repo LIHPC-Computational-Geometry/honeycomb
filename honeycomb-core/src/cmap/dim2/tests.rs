22921e5a97257db308cd8062eddcb500132d1376
imrn99 <95699343+imrn99@users.noreply.github.com>
2025-01-20 09:12:00 +0100

    rince repeat for 2D

--- honeycomb-core/src/cmap/dim2/tests.rs
+++ honeycomb-core/src/cmap/dim2/tests.rs
@@ -4,7 +4,7 @@ use stm::atomically;
 
 use crate::{
     attributes::AttrSparseVec,
-    cmap::VertexIdType,
+    cmap::{CMapError, VertexIdType},
     prelude::{AttributeBind, AttributeUpdate, CMap2, CMapBuilder, Orbit2, OrbitPolicy, Vertex2},
 };
 
@@ -376,7 +376,6 @@ fn one_sew_no_attributes_bis() {
 
 // --- IO
 
-#[cfg(feature = "io")]
 #[test]
 fn io_write() {
     // build a map looking like this:
@@ -508,14 +507,106 @@ fn sew_ordering() {
         t2.join().unwrap();
 
         // all path should result in the same topological result here
-        assert!(arc.force_read_vertex(2).is_some());
-        assert!(arc.force_read_vertex(3).is_none());
-        assert!(arc.force_read_vertex(5).is_none());
+        let v2 = arc.force_remove_vertex(2);
+        let v3 = arc.force_remove_vertex(3);
+        let v5 = arc.force_remove_vertex(5);
+        assert!(v2.is_some());
+        assert!(v3.is_none());
+        assert!(v5.is_none());
         assert_eq!(Orbit2::new(arc.as_ref(), OrbitPolicy::Vertex, 2).count(), 3);
+        assert_eq!(arc.force_read_vertex(2), None);
+        assert_eq!(arc.force_read_vertex(3), None);
+        assert_eq!(arc.force_read_vertex(5), None);
 
-        // the v2 can have two values though
-        let path1 = arc.force_read_vertex(2) == Some(Vertex2(1.5, 1.75));
-        let path2 = arc.force_read_vertex(2) == Some(Vertex2(1.25, 1.5));
+        // v2 can have two values though
+        let path1 = v2 == Some(Vertex2(1.5, 1.75));
+        let path2 = v2 == Some(Vertex2(1.25, 1.5));
+        assert!(path1 || path2);
+    });
+}
+
+#[test]
+fn sew_ordering_with_transactions() {
+    loom::model(|| {
+        // setup the map
+        let map: CMap2<f64> = CMapBuilder::default().n_darts(5).build().unwrap();
+        atomically(|trans| {
+            map.link::<2>(trans, 1, 2)?;
+            map.link::<1>(trans, 4, 5)?;
+            map.write_vertex(trans, 2, Vertex2(1.0, 1.0))?;
+            map.write_vertex(trans, 3, Vertex2(1.0, 2.0))?;
+            map.write_vertex(trans, 5, Vertex2(2.0, 2.0))?;
+            Ok(())
+        });
+
+        let arc = loom::sync::Arc::new(map);
+        let (m1, m2) = (arc.clone(), arc.clone());
+
+        // we're going to do to sew ops:
+        // - 1-sew 1 to 3 (t1)
+        // - 2-sew 3 to 4 (t2)
+        // this will result in a single vertex being define, of ID 2
+        // depending on the order of execution of the sews, the value may change
+        // 1-sew before 2-sew: (1.5, 1.75)
+        // 2-sew before 1-sew: (1.25, 1.5)
+
+        let t1 = loom::thread::spawn(move || {
+            atomically(|trans| {
+                if let Err(e) = m1.sew::<1>(trans, 1, 3) {
+                    match e {
+                        CMapError::FailedTransaction(e) => Err(e),
+                        CMapError::FailedAttributeMerge(_) => Err(stm::StmError::Retry),
+                        CMapError::FailedAttributeSplit(_)
+                        | CMapError::IncorrectGeometry(_)
+                        | CMapError::UnknownAttribute(_) => unreachable!(),
+                    }
+                } else {
+                    Ok(())
+                }
+            });
+        });
+
+        let t2 = loom::thread::spawn(move || {
+            atomically(|trans| {
+                if let Err(e) = m2.sew::<2>(trans, 3, 4) {
+                    match e {
+                        CMapError::FailedTransaction(e) => Err(e),
+                        CMapError::FailedAttributeMerge(_) => Err(stm::StmError::Retry),
+                        CMapError::FailedAttributeSplit(_)
+                        | CMapError::IncorrectGeometry(_)
+                        | CMapError::UnknownAttribute(_) => unreachable!(),
+                    }
+                } else {
+                    Ok(())
+                }
+            });
+        });
+
+        t1.join().unwrap();
+        t2.join().unwrap();
+
+        // all path should result in the same topological result here
+        let (v2, v3, v5) = atomically(|trans| {
+            Ok((
+                arc.remove_vertex(trans, 2)?,
+                arc.remove_vertex(trans, 3)?,
+                arc.remove_vertex(trans, 5)?,
+            ))
+        });
+        assert!(v2.is_some());
+        assert!(v3.is_none());
+        assert!(v5.is_none());
+        assert_eq!(Orbit2::new(arc.as_ref(), OrbitPolicy::Vertex, 2).count(), 3);
+        atomically(|trans| {
+            assert_eq!(arc.read_vertex(trans, 2)?, None);
+            assert_eq!(arc.read_vertex(trans, 3)?, None);
+            assert_eq!(arc.read_vertex(trans, 5)?, None);
+            Ok(())
+        });
+
+        // v2 can have two values though
+        let path1 = v2 == Some(Vertex2(1.5, 1.75));
+        let path2 = v2 == Some(Vertex2(1.25, 1.5));
         assert!(path1 || path2);
     });
 }
@@ -558,12 +649,110 @@ fn unsew_ordering() {
         t2.join().unwrap();
 
         // all path should result in the same topological result here
-        assert!(arc.force_read_attribute::<Weight>(2).is_some());
-        assert!(arc.force_read_attribute::<Weight>(3).is_some());
-        assert!(arc.force_read_attribute::<Weight>(5).is_some());
-        let w2 = arc.force_read_attribute::<Weight>(2).unwrap();
-        let w3 = arc.force_read_attribute::<Weight>(3).unwrap();
-        let w5 = arc.force_read_attribute::<Weight>(5).unwrap();
+        let w2 = arc.force_remove_attribute::<Weight>(2);
+        let w3 = arc.force_remove_attribute::<Weight>(3);
+        let w5 = arc.force_remove_attribute::<Weight>(5);
+        assert!(w2.is_some());
+        assert!(w3.is_some());
+        assert!(w5.is_some());
+        let w2 = w2.unwrap();
+        let w3 = w3.unwrap();
+        let w5 = w5.unwrap();
+        assert!(arc.force_read_attribute::<Weight>(2).is_none());
+        assert!(arc.force_read_attribute::<Weight>(3).is_none());
+        assert!(arc.force_read_attribute::<Weight>(5).is_none());
+
+        // check scenarios
+        let path1 = w2.0 == 17 && w3.0 == 8 && w5.0 == 8;
+        let path2 = w2.0 == 9 && w3.0 == 8 && w5.0 == 16;
+        assert!(path1 || path2);
+    });
+}
+
+#[test]
+fn unsew_ordering_with_transactions() {
+    loom::model(|| {
+        // setup the map
+        let map: CMap2<f64> = CMapBuilder::default()
+            .n_darts(5)
+            .add_attribute::<Weight>()
+            .build()
+            .unwrap();
+        atomically(|trans| {
+            map.link::<2>(trans, 1, 2)?;
+            map.link::<2>(trans, 3, 4)?;
+            map.link::<1>(trans, 1, 3)?;
+            map.link::<1>(trans, 4, 5)?;
+            map.write_vertex(trans, 2, Vertex2(0.0, 0.0))?;
+            map.write_attribute(trans, 2, Weight(33))?;
+            Ok(())
+        });
+        let arc = loom::sync::Arc::new(map);
+        let (m1, m2) = (arc.clone(), arc.clone());
+
+        // we're going to do to sew ops:
+        // - 1-unsew 1 and 3 (t1)
+        // - 2-unsew 3 and 4 (t2)
+        // this will result in 3 different weights, defined on IDs 2, 3 and 5
+        // depending on the order of execution, the final weights will take the following values:
+        // 1-unsew before 2-unsew: (W2, W3, W5) = (17, 8, 8)
+        // 2-unsew before 1-unsew: (W2, W3, W5) = (9, 8, 16)
+
+        let t1 = loom::thread::spawn(move || {
+            atomically(|trans| {
+                if let Err(e) = m1.unsew::<1>(trans, 1) {
+                    match e {
+                        CMapError::FailedTransaction(e) => Err(e),
+                        CMapError::FailedAttributeSplit(_) => Err(stm::StmError::Retry),
+                        CMapError::FailedAttributeMerge(_)
+                        | CMapError::IncorrectGeometry(_)
+                        | CMapError::UnknownAttribute(_) => unreachable!(),
+                    }
+                } else {
+                    Ok(())
+                }
+            });
+        });
+
+        let t2 = loom::thread::spawn(move || {
+            atomically(|trans| {
+                if let Err(e) = m2.unsew::<2>(trans, 3) {
+                    match e {
+                        CMapError::FailedTransaction(e) => Err(e),
+                        CMapError::FailedAttributeSplit(_) => Err(stm::StmError::Retry),
+                        CMapError::FailedAttributeMerge(_)
+                        | CMapError::IncorrectGeometry(_)
+                        | CMapError::UnknownAttribute(_) => unreachable!(),
+                    }
+                } else {
+                    Ok(())
+                }
+            });
+        });
+
+        t1.join().unwrap();
+        t2.join().unwrap();
+
+        // all path should result in the same topological result here
+        let (w2, w3, w5) = atomically(|trans| {
+            Ok((
+                arc.remove_attribute::<Weight>(trans, 2)?,
+                arc.remove_attribute::<Weight>(trans, 3)?,
+                arc.remove_attribute::<Weight>(trans, 5)?,
+            ))
+        });
+        assert!(w2.is_some());
+        assert!(w3.is_some());
+        assert!(w5.is_some());
+        let w2 = w2.unwrap();
+        let w3 = w3.unwrap();
+        let w5 = w5.unwrap();
+        atomically(|trans| {
+            assert!(arc.read_attribute::<Weight>(trans, 2)?.is_none());
+            assert!(arc.read_attribute::<Weight>(trans, 3)?.is_none());
+            assert!(arc.read_attribute::<Weight>(trans, 5)?.is_none());
+            Ok(())
+        });
 
         // check scenarios
         let path1 = w2.0 == 17 && w3.0 == 8 && w5.0 == 8;
