7710d5cdd51dab02aa189f53785c604bc3644929
imrn99 <95699343+imrn99@users.noreply.github.com>
2025-02-09 21:11:50 +0100

    fix mismatched n_darts

--- honeycomb-core/src/cmap/dim2/serialize.rs
+++ honeycomb-core/src/cmap/dim2/serialize.rs
@@ -33,7 +33,7 @@ impl<T: CoordsFloat + 'static> CMap2<T> {
             &mut file,
             "{} 2 {}",
             env!("CARGO_PKG_VERSION"), // indicates which version was used to generate the file
-            n_darts
+            n_darts - 1
         )
         .expect("E: couldn't write to file");
         writeln!(&mut file).expect("E: couldn't write to file"); // not required, but nice
