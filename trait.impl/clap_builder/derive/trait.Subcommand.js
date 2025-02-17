cdd94683ad91b10d4e93df5c7e9a3978bbcf195f
imrn99 <imrn99@users.noreply.github.com>
2025-02-17 07:31:51 +0000

    deploy: c6e6f853a51558c313f4636fb2f9ff6fabb39f9f

---
+++ trait.impl/clap_builder/derive/trait.Subcommand.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["honeycomb_benches",[["impl Subcommand for <a class=\"enum\" href=\"honeycomb_benches/cli/enum.Benches.html\" title=\"enum honeycomb_benches::cli::Benches\">Benches</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[173]}
---
+++ trait.impl/core/cmp/trait.Ord.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["honeycomb_benches",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"honeycomb_benches/cli/enum.Backend.html\" title=\"enum honeycomb_benches::cli::Backend\">Backend</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"honeycomb_benches/cli/enum.Clip.html\" title=\"enum honeycomb_benches::cli::Clip\">Clip</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"honeycomb_benches/cli/enum.Format.html\" title=\"enum honeycomb_benches::cli::Format\">Format</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"honeycomb_benches/cli/enum.Split.html\" title=\"enum honeycomb_benches::cli::Split\">Split</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1061]}
