3c38b62fae74db6643a1dee472dc29a97cd7f432
imrn99 <isaie.muron@cea.fr>
2025-03-03 15:02:52 +0100

    fix mistake

--- honeycomb-kernels/src/skewness/tests.rs
+++ honeycomb-kernels/src/skewness/tests.rs
@@ -6,7 +6,6 @@ use crate::skewness::compute_face_skewness_2d;
 fn dim2_grids() {
     // squares are equiangular
     let map: CMap2<f32> = CMapBuilder::unit_grid(2).build().unwrap();
-<<<<<<< HEAD
     assert!(
         map.iter_faces()
             .map(|fid| compute_face_skewness_2d(&map, fid))
@@ -19,10 +18,4 @@ fn dim2_grids() {
             .map(|fid| compute_face_skewness_2d(&map, fid))
             .all(|s| s == 0.25)
     );
-=======
-    assert!(compute_cells_skewness_2d(&map).all(|s| s == 0.0));
-    // triangles aren't; their angles are 90, 45, 45
-    let map: CMap2<f32> = CMapBuilder::unit_triangles(2).build().unwrap();
-    assert!(compute_cells_skewness_2d(&map).all(|s| s == 0.25));
->>>>>>> 69906560083ca04217e52dcd1a55a67ff2a4206f
 }
