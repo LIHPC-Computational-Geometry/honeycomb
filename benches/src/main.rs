6dfb1ffa97c458d013e8677da9fa53930c44fa19
imrn99 <isaie.muron@cea.fr>
2025-05-16 15:03:12 +0200

    feat(benches): implement thread binding for `cut-edges` std threads backend

--- benches/src/main.rs
+++ benches/src/main.rs
@@ -1,18 +1,15 @@
 use std::io::Write;
 #[cfg(feature = "thread-binding")]
-use std::{collections::VecDeque, sync::Arc};
+use std::sync::Arc;
 
 use clap::Parser;
 use honeycomb::prelude::{CMap2, CoordsFloat};
 #[cfg(feature = "thread-binding")]
-use hwlocality::{
-    Topology,
-    cpu::binding::CpuBindingFlags,
-    object::types::ObjectType,
-    topology::support::{DiscoverySupport, FeatureSupport},
-};
+use hwlocality::{Topology, cpu::binding::CpuBindingFlags};
 use rayon::ThreadPoolBuilder;
 
+#[cfg(feature = "thread-binding")]
+use honeycomb_benches::utils::get_physical_cores;
 use honeycomb_benches::{
     cli::{Benches, Cli, Format},
     cut_edges::bench_cut_edges,
@@ -28,7 +25,9 @@ fn main() {
     let n_t = if let Some(val) = cli.n_threads {
         val.get()
     } else {
-        std::thread::available_parallelism().unwrap().get()
+        std::thread::available_parallelism()
+            .map(|v| v.get())
+            .unwrap_or(1)
     };
     let builder = ThreadPoolBuilder::new().num_threads(n_t);
 
@@ -38,17 +37,8 @@ fn main() {
             // build the topology & check that all necessary features are available on the machine
             let topology = Topology::new().unwrap();
             let topology = Arc::new(topology);
-            if topology.supports(FeatureSupport::discovery, DiscoverySupport::pu_count) {
-                let cpu_bind_feats = topology
-                    .feature_support()
-                    .cpu_binding()
-                    .is_some_and(|s| s.get_thread() && s.set_thread());
-                if cpu_bind_feats {
-                    // configure the global thread pool
-                    let core_depth = topology.depth_or_below_for_type(ObjectType::Core).unwrap();
-                    let mut cores = topology
-                        .objects_at_depth(core_depth)
-                        .collect::<VecDeque<_>>();
+            match get_physical_cores(&topology) {
+                Ok(mut cores) => {
                     let n_t = if cores.len() < n_t {
                         // don't allow more than one thread per physical core
                         // this is sane since this branch executes only if we explicitly enable binding
@@ -83,18 +73,14 @@ fn main() {
                         })
                         .build_global()
                         .unwrap();
-                } else {
-                    eprintln!(
-                        "W: Missing CPU binding support; proceeding with the default rayon threadpool"
-                    );
+                }
+                Err(e) => {
+                    eprintln!("W: {e}");
                     builder.build_global().unwrap();
                 }
-            } else {
-                eprintln!(
-                    "W: Missing PU reporting support; proceeding with the default rayon threadpool"
-                );
-                builder.build_global().unwrap();
             }
+        } else {
+            builder.build_global().unwrap();
         }
     }
     #[cfg(not(feature = "thread-binding"))]
@@ -103,16 +89,24 @@ fn main() {
     }
 
     if cli.simple_precision {
-        run_benchmarks::<f32>(cli);
+        run_benchmarks::<f32>(cli, n_t);
     } else {
-        run_benchmarks::<f64>(cli);
+        run_benchmarks::<f64>(cli, n_t);
     }
 }
 
-fn run_benchmarks<T: CoordsFloat>(cli: Cli) {
+fn run_benchmarks<T: CoordsFloat>(cli: Cli, n_threads: usize) {
+    #[allow(unused_mut, unused_assignments)]
+    let mut bind_threads = false;
+
+    #[cfg(feature = "thread-binding")]
+    {
+        bind_threads = cli.bind_threads;
+    }
+
     let map: CMap2<T> = match cli.benches {
         Benches::Generate2dGrid(args) => bench_generate_2d_grid(args),
-        Benches::CutEdges(args) => bench_cut_edges(args),
+        Benches::CutEdges(args) => bench_cut_edges(args, n_threads, bind_threads),
         Benches::Grisubal(args) => bench_grisubal(args),
         Benches::Remesh(args) => bench_remesh(args),
         Benches::Shift(args) => bench_shift(args),
--- benches/src/utils.rs
+++ benches/src/utils.rs
@@ -1,7 +1,16 @@
+#[cfg(feature = "thread-binding")]
+use std::collections::VecDeque;
 use std::collections::hash_map::DefaultHasher;
 use std::fs::File;
 use std::hash::Hasher;
 use std::io::Read;
+
+#[cfg(feature = "thread-binding")]
+use hwlocality::{
+    Topology,
+    object::{TopologyObject, types::ObjectType},
+    topology::support::{DiscoverySupport, FeatureSupport},
+};
 
 cfg_if::cfg_if! {
     if #[cfg(feature = "_single_precision")] {
@@ -31,4 +40,39 @@ pub fn hash_file(path: &str) -> Result<u64, std::io::Error> {
     }
 
     Ok(hasher.finish())
+}
+
+#[cfg(feature = "thread-binding")]
+pub fn get_physical_cores<'a, 'b>(
+    topology: &'a Topology,
+) -> Result<VecDeque<&'b TopologyObject>, String>
+where
+    'a: 'b,
+{
+    if topology.supports(FeatureSupport::discovery, DiscoverySupport::pu_count) {
+        let cpu_bind_feats = topology
+            .feature_support()
+            .cpu_binding()
+            .is_some_and(|s| s.get_thread() && s.set_thread());
+        if cpu_bind_feats {
+            // configure the global thread pool
+            let core_depth = topology
+                .depth_or_below_for_type(ObjectType::Core)
+                .map_err(|e| e.to_string())?;
+            let cores = topology
+                .objects_at_depth(core_depth)
+                .collect::<VecDeque<_>>();
+            Ok(cores)
+        } else {
+            Err(
+                "Missing CPU binding support; proceeding with the default rayon threadpool"
+                    .to_string(),
+            )
+        }
+    } else {
+        Err(
+            "Missing PU reporting support; proceeding with the default rayon threadpool"
+                .to_string(),
+        )
+    }
 }
