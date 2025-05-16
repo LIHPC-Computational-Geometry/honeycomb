50dbb52c7550e27fd6bfcfcfe97544a0654b2517
imrn99 <isaie.muron@cea.fr>
2025-05-16 10:25:03 +0200

    feat(bench): add thread binding logic to the rayon threadpool

--- benches/src/main.rs
+++ benches/src/main.rs
@@ -1,7 +1,16 @@
-use std::io::Write;
+use std::{collections::VecDeque, io::Write, sync::Arc};
 
 use clap::Parser;
 use honeycomb::prelude::{CMap2, CoordsFloat};
+#[cfg(feature = "thread-binding")]
+use hwlocality::{
+    Topology,
+    cpu::binding::CpuBindingFlags,
+    object::types::ObjectType,
+    topology::support::{DiscoverySupport, FeatureSupport},
+};
+#[cfg(feature = "thread-binding")]
+use rayon::ThreadPoolBuilder;
 
 use honeycomb_benches::{
     cli::{Benches, Cli, Format},
@@ -14,6 +23,79 @@ use honeycomb_benches::{
 
 fn main() {
     let cli = Cli::parse();
+
+    let n_t = if let Some(val) = cli.n_threads {
+        val.get()
+    } else {
+        std::thread::available_parallelism().unwrap().get()
+    };
+    let builder = ThreadPoolBuilder::new().num_threads(n_t);
+
+    #[cfg(feature = "thread-binding")]
+    {
+        if cli.bind_threads {
+            // build the topology & check that all necessary features are available on the machine
+            let topology = Topology::new().unwrap();
+            let topology = Arc::new(topology);
+            if topology.supports(FeatureSupport::discovery, DiscoverySupport::pu_count) {
+                let cpu_bind_feats = topology
+                    .feature_support()
+                    .cpu_binding()
+                    .is_some_and(|s| s.get_thread() && s.set_thread());
+                if cpu_bind_feats {
+                    // configure the global thread pool
+                    let core_depth = topology.depth_or_below_for_type(ObjectType::Core).unwrap();
+                    let mut cores = topology
+                        .objects_at_depth(core_depth)
+                        .collect::<VecDeque<_>>();
+                    let n_t = if cores.len() < n_t {
+                        // don't allow more than one thread per physical core
+                        // this is sane since this branch executes only if we explicitly enable binding
+                        eprintln!(
+                            "W: Less physical cores than logical threads; proceeding with one thread per core ({})",
+                            cores.len()
+                        );
+                        cores.len()
+                    } else {
+                        n_t
+                    };
+                    builder
+                        .num_threads(n_t)
+                        .spawn_handler(|t_builder| {
+                            // master thread
+                            let topology = topology.clone();
+                            // safe to unwrap due to n_t value adjustment
+                            let core = cores.pop_front().expect("E: unreachable");
+                            let mut bind_to = core.cpuset().unwrap().clone_target();
+                            bind_to.singlify();
+                            std::thread::spawn(move || {
+                                // worker thread
+                                let tid = hwlocality::current_thread_id();
+                                topology
+                                    .bind_thread_cpu(tid, &bind_to, CpuBindingFlags::empty())
+                                    .unwrap();
+
+                                // do the work
+                                t_builder.run()
+                            });
+                            Ok(())
+                        })
+                        .build_global()
+                        .unwrap();
+                } else {
+                    eprintln!(
+                        "W: Missing CPU binding support; proceeding with the default rayon threadpool"
+                    );
+                    builder.build_global().unwrap();
+                }
+            } else {
+                eprintln!(
+                    "W: Missing PU reporting support; proceeding with the default rayon threadpool"
+                );
+                builder.build_global().unwrap();
+            }
+        }
+    }
 
     if cli.simple_precision {
         run_benchmarks::<f32>(cli);
