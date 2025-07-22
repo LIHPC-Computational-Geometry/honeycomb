ceb8d35022b0ac3d96a05946a61210b458ce545c
imrn99 <github@imrn.fr>
2025-07-22 16:31:35 +0200

    fix: format

--- honeycomb-core/src/cmap/builder/io.rs
+++ honeycomb-core/src/cmap/builder/io.rs
@@ -373,7 +373,7 @@ pub fn build_2d_from_vtk<T: CoordsFloat>(
                                         cmap.force_link::<1>(d0, d1).unwrap(); // edge d0 links vertices vids[0] & vids[1]
                                         cmap.force_link::<1>(d1, d2).unwrap(); // edge d1 links vertices vids[1] & vids[2]
                                         cmap.force_link::<1>(d2, d0).unwrap(); // edge d2 links vertices vids[2] & vids[0]
-                                                                               // record a trace of the built cell for future 2-sew
+                                        // record a trace of the built cell for future 2-sew
                                         sew_buffer.insert((vids[0], vids[1]), d0);
                                         sew_buffer.insert((vids[1], vids[2]), d1);
                                         sew_buffer.insert((vids[2], vids[0]), d2);
@@ -434,7 +434,7 @@ pub fn build_2d_from_vtk<T: CoordsFloat>(
                                         cmap.force_link::<1>(d1, d2).unwrap(); // edge d1 links vertices vids[1] & vids[2]
                                         cmap.force_link::<1>(d2, d3).unwrap(); // edge d2 links vertices vids[2] & vids[3]
                                         cmap.force_link::<1>(d3, d0).unwrap(); // edge d3 links vertices vids[3] & vids[0]
-                                                                               // record a trace of the built cell for future 2-sew
+                                        // record a trace of the built cell for future 2-sew
                                         sew_buffer.insert((vids[0], vids[1]), d0);
                                         sew_buffer.insert((vids[1], vids[2]), d1);
                                         sew_buffer.insert((vids[2], vids[3]), d2);
