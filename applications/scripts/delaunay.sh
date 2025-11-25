dff1e6cbcb7dc6c70422583711e37fec1968f50b
imrn99 <95699343+imrn99@users.noreply.github.com>
2025-11-25 15:37:24 +0100

    feat(apps): add conditional spatial sort to Delaunay script
    it uses the SORT environment variable to then pass the correct option to
    the binary

--- applications/scripts/delaunay.sh
+++ applications/scripts/delaunay.sh
@@ -5,6 +5,10 @@ TARGET_DIR=${HCWORKDIR}/target/release
 
 mkdir out
 
+if [ -n "${SORT}" ]; then
+  SPATIAL_SORT=--enable-spatial-sort
+fi
+
 # Delaunay (sequential)
 #
 # - STM overhead/cost
@@ -14,6 +18,7 @@ ${TARGET_DIR}/incremental-delaunay \
     10 10 10 \
     32 \
     --init-points 1024 \
+    ${SPATIAL_SORT} \
     >> out/delaunay_seq.out
 
 # Delaunay (weak scaling, minimal triangulation)
@@ -26,30 +31,37 @@ ${TARGET_DIR}/incremental-delaunay \
 RAYON_NUM_THREADS=1 ${TARGET_DIR}/incremental-delaunay \
     10 10 10 \
     2048 \
+    ${SPATIAL_SORT} \
     >> out/delaunay_min_1.out
 RAYON_NUM_THREADS=3 ${TARGET_DIR}/incremental-delaunay \
     10 10 10 \
     6144 \
+    ${SPATIAL_SORT} \
     >> out/delaunay_min_3.out
 RAYON_NUM_THREADS=9 ${TARGET_DIR}/incremental-delaunay \
     10 10 10 \
     18432 \
+    ${SPATIAL_SORT} \
     >> out/delaunay_min_9.out
 RAYON_NUM_THREADS=18 ${TARGET_DIR}/incremental-delaunay \
     10 10 10 \
     36864 \
+    ${SPATIAL_SORT} \
     >> out/delaunay_min_18.out
 RAYON_NUM_THREADS=36 ${TARGET_DIR}/incremental-delaunay \
     10 10 10 \
     73728 \
+    ${SPATIAL_SORT} \
     >> out/delaunay_min_36.out
 RAYON_NUM_THREADS=54 ${TARGET_DIR}/incremental-delaunay \
     10 10 10 \
     110592 \
+    ${SPATIAL_SORT} \
     >> out/delaunay_min_54.out
 RAYON_NUM_THREADS=72 ${TARGET_DIR}/incremental-delaunay \
     10 10 10 \
     147456 \
+    ${SPATIAL_SORT} \
     >> out/delaunay_min_72.out
 
 # Delaunay (weak scaling, large triangulation)
@@ -63,36 +75,43 @@ RAYON_NUM_THREADS=1 ${TARGET_DIR}/incremental-delaunay \
     10 10 10 \
     2048 \
     --init-points 512 \
+    ${SPATIAL_SORT} \
     >> out/delaunay_sc_1.out
 RAYON_NUM_THREADS=3 ${TARGET_DIR}/incremental-delaunay \
     10 10 10 \
     6144 \
     --init-points 1536 \
+    ${SPATIAL_SORT} \
     >> out/delaunay_sc_3.out
 RAYON_NUM_THREADS=9 ${TARGET_DIR}/incremental-delaunay \
     10 10 10 \
     18432 \
     --init-points 4608 \
+    ${SPATIAL_SORT} \
     >> out/delaunay_sc_9.out
 RAYON_NUM_THREADS=18 ${TARGET_DIR}/incremental-delaunay \
     10 10 10 \
     36864 \
     --init-points 9216 \
+    ${SPATIAL_SORT} \
     >> out/delaunay_sc_18.out
 RAYON_NUM_THREADS=36 ${TARGET_DIR}/incremental-delaunay \
     10 10 10 \
     73728 \
     --init-points 18432 \
+    ${SPATIAL_SORT} \
     >> out/delaunay_sc_36.out
 RAYON_NUM_THREADS=54 ${TARGET_DIR}/incremental-delaunay \
     10 10 10 \
     110592 \
     --init-points 27648 \
+    ${SPATIAL_SORT} \
     >> out/delaunay_sc_54.out
 RAYON_NUM_THREADS=72 ${TARGET_DIR}/incremental-delaunay \
     10 10 10 \
     147456 \
     --init-points 36864 \
+    ${SPATIAL_SORT} \
     >> out/delaunay_sc_72.out
 
 # 1 -> 288
