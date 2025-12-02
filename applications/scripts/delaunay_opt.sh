8fdb0712b7b457b0e78deee39868dd0c5f0b82ac
imrn99 <95699343+imrn99@users.noreply.github.com>
2025-12-02 10:51:03 +0100

    refactor(apps): split Delaunay bench script to extract best setup

--- applications/scripts/delaunay_opt.sh
+++ applications/scripts/delaunay_opt.sh
@@ -5,8 +5,10 @@ TARGET_DIR=${HCWORKDIR}/target/release
 
 mkdir out
 
-if [ -n "${SORT}" ]; then
-  SPATIAL_SORT=--enable-spatial-sort
+SPATIAL_SORT=--enable-spatial-sort
+
+if [ -n "${DISABLE_SORT}" ]; then
+  unset SPATIAL_SORT
 fi
 
 # Delaunay (sequential)
@@ -21,50 +23,12 @@ ${TARGET_DIR}/incremental-delaunay \
     ${SPATIAL_SORT} \
     >> out/delaunay_seq.out
 
-# Delaunay (weak scaling, minimal triangulation)
+
+# Delaunay (weak scaling, large starting triangulation)
 #
-# - conflict cost
-# - algorithm perf
-
-# 1 -> 72
-
-RAYON_NUM_THREADS=1 ${TARGET_DIR}/incremental-delaunay \
-    10 10 10 \
-    2048 \
-    ${SPATIAL_SORT} \
-    >> out/delaunay_min_1.out
-RAYON_NUM_THREADS=3 ${TARGET_DIR}/incremental-delaunay \
-    10 10 10 \
-    6144 \
-    ${SPATIAL_SORT} \
-    >> out/delaunay_min_3.out
-RAYON_NUM_THREADS=9 ${TARGET_DIR}/incremental-delaunay \
-    10 10 10 \
-    18432 \
-    ${SPATIAL_SORT} \
-    >> out/delaunay_min_9.out
-RAYON_NUM_THREADS=18 ${TARGET_DIR}/incremental-delaunay \
-    10 10 10 \
-    36864 \
-    ${SPATIAL_SORT} \
-    >> out/delaunay_min_18.out
-RAYON_NUM_THREADS=36 ${TARGET_DIR}/incremental-delaunay \
-    10 10 10 \
-    73728 \
-    ${SPATIAL_SORT} \
-    >> out/delaunay_min_36.out
-RAYON_NUM_THREADS=54 ${TARGET_DIR}/incremental-delaunay \
-    10 10 10 \
-    110592 \
-    ${SPATIAL_SORT} \
-    >> out/delaunay_min_54.out
-RAYON_NUM_THREADS=72 ${TARGET_DIR}/incremental-delaunay \
-    10 10 10 \
-    147456 \
-    ${SPATIAL_SORT} \
-    >> out/delaunay_min_72.out
-
-# Delaunay (weak scaling, large triangulation)
+# # of threads    | 1    | 3    | 9     | 18    | 36    | 54     | 72
+# seq. insertions | 512  | 1536 | 4608  | 9216  | 18432 | 27648  | 36864
+# par. insertions | 2048 | 6144 | 18432 | 36864 | 73728 | 110592 | 147456
 #
 # - scalability
 # - algorithm perf
