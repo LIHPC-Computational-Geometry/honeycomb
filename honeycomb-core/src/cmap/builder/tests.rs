9d8ad7f5c94c2d51bd3e736491e8b1385eb58bf9
imrn99 <95699343+imrn99@users.noreply.github.com>
2025-12-15 09:50:18 +0100

    tests(core): update version header in deserialization test

--- honeycomb-core/src/cmap/builder/tests.rs
+++ honeycomb-core/src/cmap/builder/tests.rs
@@ -110,11 +110,11 @@ mod cmap {
 
     #[cfg(test)]
     const BAD_METAS: [&str; 9] = [
-        "0.10.0 2",               // 2 elems
-        "0.10.0 2 18 23",         // 4 elems
-        "0.10.0 2.5 18",          // bad dim
-        "0.10.0 2 hi",            // bad darts
-        "0.10.0 bye 18",          // bad dim again
+        "0.10.1 2",               // 2 elems
+        "0.10.1 2 18 23",         // 4 elems
+        "0.10.1 2.5 18",          // bad dim
+        "0.10.1 2 hi",            // bad darts
+        "0.10.1 bye 18",          // bad dim again
         "super super bad header", // ...
         "  ",                     // "" + ' ' + "" + ' ' + ""
         "",                       // empty
@@ -125,7 +125,7 @@ mod cmap {
 
     #[cfg(test)]
     const MAP: &[u8] = b"[META]
-0.10.0 2 4
+0.10.1 2 4
 
 [BETAS]
 0 4 1 2 3
