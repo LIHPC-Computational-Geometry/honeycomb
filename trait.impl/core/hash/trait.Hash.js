5b3b14af292836e5a62b5bcac0b16c79699d1816
imrn99 <imrn99@users.noreply.github.com>
2026-03-06 09:27:02 +0000

    deploy: 3a4a9bdf2288ed4e79581001e595906edcf84aa1

---
+++ trait.impl/core/hash/trait.Hash.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["honeycomb_kernels",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"honeycomb_kernels/utils/enum.EdgeAnchor.html\" title=\"enum honeycomb_kernels::utils::EdgeAnchor\">EdgeAnchor</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"honeycomb_kernels/utils/enum.FaceAnchor.html\" title=\"enum honeycomb_kernels::utils::FaceAnchor\">FaceAnchor</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"honeycomb_kernels/utils/enum.VertexAnchor.html\" title=\"enum honeycomb_kernels::utils::VertexAnchor\">VertexAnchor</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[881]}
