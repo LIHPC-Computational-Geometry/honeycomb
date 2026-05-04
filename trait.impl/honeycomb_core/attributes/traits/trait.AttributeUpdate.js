acf065ee32c8add9ebfc2e24d50cc022bff20067
imrn99 <imrn99@users.noreply.github.com>
2026-05-04 05:36:12 +0000

    deploy: 180ff2994685f2a719c09c3a1378d7611428805d

---
+++ trait.impl/honeycomb_core/attributes/traits/trait.AttributeUpdate.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["honeycomb",[]],["honeycomb_core",[]],["honeycomb_kernels",[["impl <a class=\"trait\" href=\"honeycomb_core/attributes/traits/trait.AttributeUpdate.html\" title=\"trait honeycomb_core::attributes::traits::AttributeUpdate\">AttributeUpdate</a> for <a class=\"enum\" href=\"honeycomb_kernels/utils/enum.EdgeAnchor.html\" title=\"enum honeycomb_kernels::utils::EdgeAnchor\">EdgeAnchor</a>",0],["impl <a class=\"trait\" href=\"honeycomb_core/attributes/traits/trait.AttributeUpdate.html\" title=\"trait honeycomb_core::attributes::traits::AttributeUpdate\">AttributeUpdate</a> for <a class=\"enum\" href=\"honeycomb_kernels/utils/enum.FaceAnchor.html\" title=\"enum honeycomb_kernels::utils::FaceAnchor\">FaceAnchor</a>",0],["impl <a class=\"trait\" href=\"honeycomb_core/attributes/traits/trait.AttributeUpdate.html\" title=\"trait honeycomb_core::attributes::traits::AttributeUpdate\">AttributeUpdate</a> for <a class=\"enum\" href=\"honeycomb_kernels/utils/enum.VertexAnchor.html\" title=\"enum honeycomb_kernels::utils::VertexAnchor\">VertexAnchor</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[16,22,1020]}
